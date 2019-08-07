6e00e7d117b3a83d70de4a95c9283a6c721b22d6
Marco van den Oever <marcoo@infosupport.com>
2019-08-07 22:45:01 +0200

    Initial commit

---
+++ windows-terminal-quake/Native/HotKeyManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsTerminalQuake.Native
+{
+    public static class HotKeyManager
+    {
+        public static event EventHandler<HotKeyEventArgs> HotKeyPressed;
+
+        public static int RegisterHotKey(Keys key, KeyModifiers modifiers)
+        {
+            _windowReadyEvent.WaitOne();
+            int id = Interlocked.Increment(ref _id);
+            _wnd.Invoke(new RegisterHotKeyDelegate(RegisterHotKeyInternal), _hwnd, id, (uint)modifiers, (uint)key);
+            return id;
+        }
+
+        public static void UnregisterHotKey(int id)
+        {
+            _wnd.Invoke(new UnRegisterHotKeyDelegate(UnRegisterHotKeyInternal), _hwnd, id);
+        }
+
+        private delegate void RegisterHotKeyDelegate(IntPtr hwnd, int id, uint modifiers, uint key);
+
+        private delegate void UnRegisterHotKeyDelegate(IntPtr hwnd, int id);
+
+        private static void RegisterHotKeyInternal(IntPtr hwnd, int id, uint modifiers, uint key)
+        {
+            RegisterHotKey(hwnd, id, modifiers, key);
+        }
+
+        private static void UnRegisterHotKeyInternal(IntPtr hwnd, int id)
+        {
+            UnregisterHotKey(_hwnd, id);
+        }
+
+        private static void OnHotKeyPressed(HotKeyEventArgs e)
+        {
+            HotKeyManager.HotKeyPressed?.Invoke(null, e);
+        }
+
+        private static volatile MessageWindow _wnd;
+        private static volatile IntPtr _hwnd;
+        private static ManualResetEvent _windowReadyEvent = new ManualResetEvent(false);
+
+        static HotKeyManager()
+        {
+            Thread messageLoop = new Thread(delegate ()
+            {
+                Application.Run(new MessageWindow());
+            });
+            messageLoop.Name = "MessageLoopThread";
+            messageLoop.IsBackground = true;
+            messageLoop.Start();
+        }
+
+        private class MessageWindow : Form
+        {
+            public MessageWindow()
+            {
+                _wnd = this;
+                _hwnd = this.Handle;
+                _windowReadyEvent.Set();
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                if (m.Msg == WM_HOTKEY)
+                {
+                    HotKeyEventArgs e = new HotKeyEventArgs(m.LParam);
+                    OnHotKeyPressed(e);
+                }
+
+                base.WndProc(ref m);
+            }
+
+            protected override void SetVisibleCore(bool value)
+            {
+                // Ensure the window never becomes visible
+                base.SetVisibleCore(false);
+            }
+
+            private const int WM_HOTKEY = 0x312;
+        }
+
+        [DllImport("user32", SetLastError = true)]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32", SetLastError = true)]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private static int _id = 0;
+    }
+
+    public class HotKeyEventArgs : EventArgs
+    {
+        public readonly System.Windows.Forms.Keys Key;
+        public readonly KeyModifiers Modifiers;
+
+        public HotKeyEventArgs(System.Windows.Forms.Keys key, KeyModifiers modifiers)
+        {
+            this.Key = key;
+            this.Modifiers = modifiers;
+        }
+
+        public HotKeyEventArgs(IntPtr hotKeyParam)
+        {
+            uint param = (uint)hotKeyParam.ToInt64();
+            Key = (System.Windows.Forms.Keys)((param & 0xffff0000) >> 16);
+            Modifiers = (KeyModifiers)(param & 0x0000ffff);
+        }
+    }
+
+    [Flags]
+    public enum KeyModifiers
+    {
+        Alt = 1,
+        Control = 2,
+        Shift = 4,
+        Windows = 8,
+        NoRepeat = 0x4000
+    }
+}
---
+++ windows-terminal-quake/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsTerminalQuake.Native;
+
+namespace WindowsTerminalQuake
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var process = Process.GetProcessesByName("WindowsTerminal").FirstOrDefault();
+            if (process == null)
+            {
+                Console.WriteLine("No WindowsTerminal process found");
+                return;
+            }
+
+            var screen = Screen.PrimaryScreen;
+            var bounds = screen.Bounds;
+
+            // Hide from taskbar
+            User32.SetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE, (User32.GetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE) | User32.WS_EX_TOOLWINDOW) & ~User32.WS_EX_APPWINDOW);
+
+            User32.Rect rect = default;
+            var ok = User32.GetWindowRect(process.MainWindowHandle, ref rect);
+            var isOpen = rect.Top >= bounds.Y;
+
+            HotKeyManager.RegisterHotKey(Keys.Oemtilde, KeyModifiers.Control);
+            HotKeyManager.HotKeyPressed += (s, a) =>
+            {
+                if (isOpen)
+                {
+                    isOpen = false;
+                    Console.WriteLine("Close");
+
+                    User32.ShowWindow(process.MainWindowHandle, NCmdShow.RESTORE);
+                    User32.SetForegroundWindow(process.MainWindowHandle);
+
+                    for (int i = 9; i >= 0; i--)
+                    {
+                        User32.MoveWindow(process.MainWindowHandle, bounds.X, bounds.Y + (-bounds.Height + (bounds.Height / 10 * i)), bounds.Width, bounds.Height, true);
+
+                        Task.Delay(1).GetAwaiter().GetResult();
+                    }
+                }
+                else
+                {
+                    isOpen = true;
+                    Console.WriteLine("Open");
+
+                    User32.ShowWindow(process.MainWindowHandle, NCmdShow.RESTORE);
+                    User32.SetForegroundWindow(process.MainWindowHandle);
+
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        User32.MoveWindow(process.MainWindowHandle, bounds.X, bounds.Y + (-bounds.Height + (bounds.Height / 10 * i)), bounds.Width, bounds.Height, true);
+
+                        Task.Delay(1).GetAwaiter().GetResult();
+                    }
+                }
+            };
+
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
+
+            // Restore taskbar icon
+            User32.SetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE, (User32.GetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE) | User32.WS_EX_TOOLWINDOW) & User32.WS_EX_APPWINDOW);
+
+            // Reset position
+            User32.MoveWindow(process.MainWindowHandle, bounds.X, bounds.Y, bounds.Width, bounds.Height, true);
+        }
+    }
+}
