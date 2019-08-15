630cb54edee677a19ed2ee4cb410e8b685fa06dc
Marco van den Oever <marco@flyingpie.nl>
2019-08-15 14:42:41 +0200

    Tray icon

---
+++ windows-terminal-quake/Toggler.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsTerminalQuake.Native;
+
+namespace WindowsTerminalQuake
+{
+    public class Toggler : IDisposable
+    {
+        private Process _process;
+
+        public Toggler()
+        {
+            _process = Process.GetProcessesByName("WindowsTerminal").FirstOrDefault();
+            if (_process == null)
+            {
+                throw new Exception("No WindowsTerminal process found");
+            }
+
+            // Hide from taskbar
+            User32.SetWindowLong(_process.MainWindowHandle, User32.GWL_EX_STYLE, (User32.GetWindowLong(_process.MainWindowHandle, User32.GWL_EX_STYLE) | User32.WS_EX_TOOLWINDOW) & ~User32.WS_EX_APPWINDOW);
+
+            User32.Rect rect = default;
+            var ok = User32.GetWindowRect(_process.MainWindowHandle, ref rect);
+            var isOpen = rect.Top >= GetScreenWithCursor().Bounds.Y;
+
+            var stepCount = 10;
+
+            HotKeyManager.RegisterHotKey(Keys.Oemtilde, KeyModifiers.Control);
+            HotKeyManager.RegisterHotKey(Keys.Q, KeyModifiers.Control);
+
+            HotKeyManager.HotKeyPressed += (s, a) =>
+            {
+                if (isOpen)
+                {
+                    isOpen = false;
+                    Console.WriteLine("Close");
+
+                    User32.ShowWindow(_process.MainWindowHandle, NCmdShow.RESTORE);
+                    User32.SetForegroundWindow(_process.MainWindowHandle);
+
+                    var bounds = GetScreenWithCursor().Bounds;
+
+                    for (int i = stepCount - 1; i >= 0; i--)
+                    {
+                        User32.MoveWindow(_process.MainWindowHandle, bounds.X, bounds.Y + (-bounds.Height + (bounds.Height / stepCount * i)), bounds.Width, bounds.Height, true);
+
+                        Task.Delay(1).GetAwaiter().GetResult();
+                    }
+                }
+                else
+                {
+                    isOpen = true;
+                    Console.WriteLine("Open");
+
+                    User32.ShowWindow(_process.MainWindowHandle, NCmdShow.RESTORE);
+                    User32.SetForegroundWindow(_process.MainWindowHandle);
+
+                    var bounds = GetScreenWithCursor().Bounds;
+
+                    for (int i = 1; i <= stepCount; i++)
+                    {
+                        User32.MoveWindow(_process.MainWindowHandle, bounds.X, bounds.Y + (-bounds.Height + (bounds.Height / stepCount * i)), bounds.Width, bounds.Height, true);
+
+                        Task.Delay(1).GetAwaiter().GetResult();
+                    }
+                }
+            };
+        }
+
+        public void Dispose()
+        {
+            ResetTerminal(_process);
+        }
+
+        private static Screen GetScreenWithCursor()
+        {
+            return Screen.AllScreens.FirstOrDefault(s => s.Bounds.Contains(Cursor.Position));
+        }
+
+        private static void ResetTerminal(Process process)
+        {
+            var bounds = GetScreenWithCursor().Bounds;
+
+            // Restore taskbar icon
+            User32.SetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE, (User32.GetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE) | User32.WS_EX_TOOLWINDOW) & User32.WS_EX_APPWINDOW);
+
+            // Reset position
+            User32.MoveWindow(process.MainWindowHandle, bounds.X, bounds.Y, bounds.Width, bounds.Height, true);
+        }
+    }
+}
---
+++ windows-terminal-quake/UI/TrayIcon.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsTerminalQuake.UI
+{
+    public class TrayIcon : IDisposable
+    {
+        private NotifyIcon _notificationIcon;
+
+        public TrayIcon(Action<object, EventArgs> exitHandler)
+        {
+            var waiter = new TaskCompletionSource<bool>();
+
+            var notifyThread = new Thread(delegate ()
+            {
+                var contextMenu = new ContextMenu();
+
+                // Exit
+                var mnuExit = new MenuItem("Exit");
+                mnuExit.Click += new EventHandler(exitHandler);
+
+                contextMenu.MenuItems.AddRange(new[]
+                {
+                    mnuExit
+                });
+
+                // Tray Icon
+                _notificationIcon = new NotifyIcon()
+                {
+                    Icon = CreateIcon(),
+                    ContextMenu = contextMenu,
+                    Text = "Windows Terminal Quake",
+                    Visible = true
+                };
+
+                waiter.SetResult(true);
+
+                Application.Run();
+            });
+
+            notifyThread.Start();
+
+            waiter.Task.GetAwaiter().GetResult();
+        }
+
+        public void Dispose()
+        {
+            _notificationIcon?.Dispose();
+            _notificationIcon = null;
+
+            Application.Exit();
+        }
+
+        public void Notify(ToolTipIcon type, string message)
+        {
+            _notificationIcon.ShowBalloonTip(3, $"Windows Terminal", message, type);
+        }
+
+        private static Icon CreateIcon()
+        {
+            var bitmap = _Resources.icon.ToBitmap();
+            bitmap.MakeTransparent(Color.White);
+            var icH = bitmap.GetHicon();
+            var ico = Icon.FromHandle(icH);
+            return ico;
+        }
+    }
+}
