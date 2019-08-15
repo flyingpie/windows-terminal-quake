1a82f2246013e582872b3979140a8055a7a8e1c2
Marco van den Oever <marco@flyingpie.nl>
2019-08-15 14:42:41 +0200

    Tray icon

--- windows-terminal-quake/Program.cs
+++ windows-terminal-quake/Program.cs
@@ -1,95 +1,39 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using WindowsTerminalQuake.Native;
+using WindowsTerminalQuake.UI;
 
 namespace WindowsTerminalQuake
 {
     public class Program
     {
+        private static Toggler _toggler;
+        private static TrayIcon _trayIcon;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _trayIcon = new TrayIcon((s, a) => Close());
 
-            var process = Process.GetProcessesByName("WindowsTerminal").FirstOrDefault();
-            if (process == null)
+            try
             {
-                Console.WriteLine("No WindowsTerminal process found");
-                return;
+                _toggler = new Toggler();
+
+                _trayIcon.Notify(ToolTipIcon.Info, $"Windows Terminal Quake is running, press CTRL+~ or CTRL+Q to toggle.");
             }
-
-            // Hide from taskbar
-            User32.SetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE, (User32.GetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE) | User32.WS_EX_TOOLWINDOW) & ~User32.WS_EX_APPWINDOW);
-
-            User32.Rect rect = default;
-            var ok = User32.GetWindowRect(process.MainWindowHandle, ref rect);
-            var isOpen = rect.Top >= GetScreenWithCursor().Bounds.Y;
-
-            var stepCount = 10;
-
-            HotKeyManager.RegisterHotKey(Keys.Oemtilde, KeyModifiers.Control);
-            HotKeyManager.RegisterHotKey(Keys.Q, KeyModifiers.Control);
-
-            HotKeyManager.HotKeyPressed += (s, a) =>
+            catch (Exception ex)
             {
-                if (isOpen)
-                {
-                    isOpen = false;
-                    Console.WriteLine("Close");
+                _trayIcon.Notify(ToolTipIcon.Error, $"Cannot start: '{ex.Message}'.");
 
-                    User32.ShowWindow(process.MainWindowHandle, NCmdShow.RESTORE);
-                    User32.SetForegroundWindow(process.MainWindowHandle);
-
-                    var bounds = GetScreenWithCursor().Bounds;
-
-                    for (int i = stepCount - 1; i >= 0; i--)
-                    {   
-                        User32.MoveWindow(process.MainWindowHandle, bounds.X, bounds.Y + (-bounds.Height + (bounds.Height / stepCount * i)), bounds.Width, bounds.Height, true);
-
-                        Task.Delay(1).GetAwaiter().GetResult();
-                    }
-                }
-                else
-                {
-                    isOpen = true;
-                    Console.WriteLine("Open");
-
-                    User32.ShowWindow(process.MainWindowHandle, NCmdShow.RESTORE);
-                    User32.SetForegroundWindow(process.MainWindowHandle);
-
-                    var bounds = GetScreenWithCursor().Bounds;
-
-                    for (int i = 1; i <= stepCount; i++)
-                    {
-                        User32.MoveWindow(process.MainWindowHandle, bounds.X, bounds.Y + (-bounds.Height + (bounds.Height / stepCount * i)), bounds.Width, bounds.Height, true);
-
-                        Task.Delay(1).GetAwaiter().GetResult();
-                    }
-                }
-            };
-
-            Console.WriteLine("Press any key to exit");
-            Console.Read();
-
-            ResetTerminal(process);
+                Close();
+            }
         }
 
-        private static void ResetTerminal(Process process)
+        private static void Close()
         {
-            var bounds = GetScreenWithCursor().Bounds;
+            _toggler?.Dispose();
+            _toggler = null;
 
-            // Restore taskbar icon
-            User32.SetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE, (User32.GetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE) | User32.WS_EX_TOOLWINDOW) & User32.WS_EX_APPWINDOW);
-
-            // Reset position
-            User32.MoveWindow(process.MainWindowHandle, bounds.X, bounds.Y, bounds.Width, bounds.Height, true);
-        }
-
-        public static Screen GetScreenWithCursor()
-        {
-            return Screen.AllScreens.FirstOrDefault(s => s.Bounds.Contains(Cursor.Position));
+            _trayIcon?.Dispose();
+            _trayIcon = null;
         }
     }
 }
