511cdfb20ea25e47c0d6ca8972a7b93c50c504b4
James Treworgy <jamietre@gmail.com>
2019-10-08 14:19:36 -0400

    When launching windows-terminal-quake when another process is already running, set terminal to foreground

--- windows-terminal-quake/Program.cs
+++ windows-terminal-quake/Program.cs
@@ -16,9 +16,17 @@ namespace WindowsTerminalQuake
         public static void Main(string[] args)
         {
             // don't allow more than one instance to be running simultaneously
-            var existingProcesses = Process.GetProcessesByName("windows-terminal-quake").Count();
-            if (existingProcesses > 1)
+            var existingProcesses = Process.GetProcessesByName("windows-terminal-quake");
+            if (existingProcesses.Count() > 1)
             {
+                try
+                {
+                    var process = GetWindowsTerminalProcess();
+                    if (process != null)
+                    {
+                        User32.SetForegroundWindow(process.MainWindowHandle);
+                    }
+                } catch (Exception) { }
                 return;
             }
 
@@ -37,53 +45,62 @@ namespace WindowsTerminalQuake
 
             try
             {
-                var processes = Process.GetProcessesByName("WindowsTerminal").Where(e => e.MainWindowHandle != default(IntPtr)).ToArray();
-                Process process = null;
-                int i = 0;
-                while (i < processes.Length)
-                {
-                    try
-                    {
-                        process = processes[i];
-                        bindProcessEvents(process);
-                        break;
-                    } catch (Exception)
-                    {
-                        i++;
-                    }
-                }
 
+                var process = GetWindowsTerminalProcess();
                 if (process == null)
                 {
-                    process = new Process
-                    {
-                        StartInfo = new ProcessStartInfo
-                        {
-                            FileName = "wt",
-                            UseShellExecute = false,
-                            RedirectStandardOutput = true,
-                            WindowStyle = config.Maximize ? ProcessWindowStyle.Maximized : ProcessWindowStyle.Hidden,
-                        }
-                    };
-                    process.Start();
-                    while (process.MainWindowTitle == ""  || process.MainWindowTitle == "DesktopWindowXamlSource")
-                    {
-                        Thread.Sleep(10);
-                        process.Refresh();
-                    }
-                    bindProcessEvents(process);
+                    process = CreateWindowsTerminalProcess(config);
                 }
-
                 _toggler = new Toggler(process, config);
-
                 _trayIcon.Notify(ToolTipIcon.Info, $"Windows Terminal Quake is running, press CTRL+~ or CTRL+Q to toggle.");
             }
             catch (Exception ex)
             {
                 _trayIcon.Notify(ToolTipIcon.Error, $"Cannot start: '{ex.Message}'.");
-
                 Close();
             }
+            
+        }
+        private static Process GetWindowsTerminalProcess()
+        {
+            var processes = Process.GetProcessesByName("WindowsTerminal").Where(e => e.MainWindowHandle != default(IntPtr)).ToArray();
+            Process process = null;
+            int i = 0;
+            while (i < processes.Length)
+            {
+                try
+                {
+                    process = processes[i];
+                    bindProcessEvents(process);
+                    break;
+                }
+                catch (Exception)
+                {
+                    i++;
+                }
+            }
+            return process;
+        }
+        private static Process CreateWindowsTerminalProcess(Config config)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "wt",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    WindowStyle = config.Maximize ? ProcessWindowStyle.Maximized : ProcessWindowStyle.Hidden,
+                }
+            };
+            process.Start();
+            while (process.MainWindowTitle == "" || process.MainWindowTitle == "DesktopWindowXamlSource")
+            {
+                Thread.Sleep(10);
+                process.Refresh();
+            }
+            bindProcessEvents(process);
+            return process;
         }
 
         private static void bindProcessEvents(Process process)
