55b9978ad9707f3f21b1e9d717c563ec7b9e5d9b
Marco van den Oever <marco@flyingpie.nl>
2020-08-22 21:35:08 +0200

    Workspace

--- windows-terminal-quake/Native/TransparentWindow.cs
+++ windows-terminal-quake/Native/TransparentWindow.cs
@@ -1,33 +1,28 @@
-﻿using System;
+﻿using Polly;
+using Polly.Retry;
+using System;
 using System.Diagnostics;
-using System.Threading.Tasks;
 
 namespace WindowsTerminalQuake.Native
 {
 	public class TransparentWindow
 	{
+		private static readonly RetryPolicy Retry = Policy
+			.Handle<Exception>()
+			.WaitAndRetry(new[] { TimeSpan.FromMilliseconds(250), TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1) })
+		;
+
 		public static void SetTransparent(Process process, int transparency)
 		{
-			Task.Run(async () =>
+			Retry.Execute(() =>
 			{
-				for (int i = 0; i < 3; i++)
-				{
-					if (process.MainWindowHandle == IntPtr.Zero)
-					{
-						await Task.Delay(TimeSpan.FromSeconds(1));
-					}
-				}
+				if (process.MainWindowHandle == IntPtr.Zero) throw new Exception("Process handle zero");
 
-				for (int i = 0; i < 3; i++)
-				{
-					var old = User32.GetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE);
-					var old2 = User32.SetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE, old | User32.WS_EX_LAYERED);
+				var old = User32.GetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE);
+				var old2 = User32.SetWindowLong(process.MainWindowHandle, User32.GWL_EX_STYLE, old | User32.WS_EX_LAYERED);
 
-					var x = User32.SetLayeredWindowAttributes(process.MainWindowHandle, 0, (byte)Math.Ceiling(255f / 100f * transparency), User32.LWA_ALPHA);
-					if (x) break;
-
-					await Task.Delay(TimeSpan.FromSeconds(1));
-				}
+				var isSet = User32.SetLayeredWindowAttributes(process.MainWindowHandle, 0, (byte)Math.Ceiling(255f / 100f * transparency), User32.LWA_ALPHA);
+				if (!isSet) throw new Exception("Could not set window opacity");
 			});
 		}
 	}
