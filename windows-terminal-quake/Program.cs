47233a895981147ff1c7d96c41055742becf4fb0
Webber Takken <webber@takken.io>
2020-09-11 01:57:37 +0200

    Fix exiting wrapper, due to inaccessible process. (#22)
    * Update editor config to reflect current code styles
    
    * Fix process permissions for some cases
    
    * Properly handle any race conditions
    
    * Resolve merge conflict
    
    * Remove no longer needed method arguments check
    
    * Simplify logging statement

--- windows-terminal-quake/Program.cs
+++ windows-terminal-quake/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Windows.Forms;
 using WindowsTerminalQuake.Native;
 using WindowsTerminalQuake.UI;
+using Serilog;
 
 namespace WindowsTerminalQuake
 {
@@ -20,22 +21,7 @@ namespace WindowsTerminalQuake
 
 			try
 			{
-				Process process = Process.GetProcessesByName("WindowsTerminal").FirstOrDefault();
-				if (process == null)
-				{
-					process = new Process
-					{
-						StartInfo = new ProcessStartInfo
-						{
-							FileName = "wt",
-							UseShellExecute = false,
-							RedirectStandardOutput = true,
-							WindowStyle = ProcessWindowStyle.Maximized
-						}
-					};
-					process.Start();
-				}
-
+				var process = GetOrCreateWindowsTerminalProcess();
 				process.EnableRaisingEvents = true;
 				process.Exited += (sender, e) =>
 				{
@@ -55,10 +41,48 @@ namespace WindowsTerminalQuake
 			}
 			catch (Exception ex)
 			{
+				Log.Logger.Warning(ex, $"Error: {ex.Message}");
 				_trayIcon.Notify(ToolTipIcon.Error, $"Cannot start: '{ex.Message}'.");
 
 				Close();
 			}
+		}
+
+		private static Process GetOrCreateWindowsTerminalProcess()
+		{
+			var process = Process.GetProcessesByName("WindowsTerminal").FirstOrDefault();
+
+			if (process == null)
+			{
+				process = new Process
+				{
+					StartInfo = new ProcessStartInfo
+					{
+						FileName = "wt",
+						UseShellExecute = false,
+						RedirectStandardOutput = true,
+						WindowStyle = ProcessWindowStyle.Maximized
+					}
+				};
+				process.Start();
+
+				try
+				{
+					// Note: Accessing mainWindowHandle already throws "Process has exited, so the requested information is not available."
+					if (process.MainWindowHandle == IntPtr.Zero)
+					{
+						throw new Exception("Can not access newly started process.");
+					}
+				}
+				catch (Exception)
+				{
+					process = Process.GetProcessesByName("WindowsTerminal").First();
+					// _process.WaitForInputIdle();
+					process.Refresh();
+				}
+			}
+
+			return process;
 		}
 
 		private static void Close()
