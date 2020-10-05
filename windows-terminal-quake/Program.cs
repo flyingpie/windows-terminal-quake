a25bf5718c8ca3800a3b5edfa59ea2005b6c6306
Marco van den Oever <marco@flyingpie.nl>
2020-10-06 01:40:28 +0200

    Fix 'Sequence contains no elements' bug

--- windows-terminal-quake/Program.cs
+++ windows-terminal-quake/Program.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Diagnostics;
+﻿using Serilog;
+using System;
 using System.Linq;
 using System.Windows.Forms;
 using WindowsTerminalQuake.Native;
 using WindowsTerminalQuake.UI;
-using Serilog;
 
 namespace WindowsTerminalQuake
 {
@@ -21,7 +20,7 @@ namespace WindowsTerminalQuake
 
 			try
 			{
-				var process = GetOrCreateWindowsTerminalProcess();
+				var process = ProcessFactory.GetOrCreateWindowsTerminalProcess();
 				process.EnableRaisingEvents = true;
 				process.Exited += (sender, e) =>
 				{
@@ -46,43 +45,6 @@ namespace WindowsTerminalQuake
 
 				Close();
 			}
-		}
-
-		private static Process GetOrCreateWindowsTerminalProcess()
-		{
-			var process = Process.GetProcessesByName("WindowsTerminal").FirstOrDefault();
-
-			if (process == null)
-			{
-				process = new Process
-				{
-					StartInfo = new ProcessStartInfo
-					{
-						FileName = "wt",
-						UseShellExecute = false,
-						RedirectStandardOutput = true,
-						WindowStyle = ProcessWindowStyle.Maximized
-					}
-				};
-				process.Start();
-
-				try
-				{
-					// Note: Accessing mainWindowHandle already throws "Process has exited, so the requested information is not available."
-					if (process.MainWindowHandle == IntPtr.Zero)
-					{
-						throw new Exception("Can not access newly started process.");
-					}
-				}
-				catch (Exception)
-				{
-					process = Process.GetProcessesByName("WindowsTerminal").First();
-					// _process.WaitForInputIdle();
-					process.Refresh();
-				}
-			}
-
-			return process;
 		}
 
 		private static void Close()
