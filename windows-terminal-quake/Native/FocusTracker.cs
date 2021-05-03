c8d70066de370dbaa5464c009a744384b18ea0db
aleybe <alexbennettece@gmail.com>
2021-05-03 22:44:01 +0800

    further update to enhancements

--- windows-terminal-quake/Native/FocusTracker.cs
+++ windows-terminal-quake/Native/FocusTracker.cs
@@ -47,47 +47,36 @@ namespace WindowsTerminalQuake.Native
 		}
 
 		// Checks to see if the current window in focus should ignore hotkey
-		public static bool HotKeySupressedForCurrentFocusedProcess()
+		public static bool HotKeySuppressedForCurrentFocusedProcess()
 		{
 			var currentWindowHandle = User32.GetForegroundWindow();
 			
 			List<Process> processes = new List<Process>();
 			foreach (var suppressedProcess in Settings.Instance.SuppressHotKeyForProcesses)
 			{
-				// need to remove the .exe
+				// Clean up the name of the processes
 				var processNameClean = suppressedProcess.Remove(
 					suppressedProcess.IndexOf(".exe", StringComparison.InvariantCultureIgnoreCase)
 					);
+				
 				var processesFound = Process.GetProcessesByName(processNameClean);
 				if (processesFound.Any())
 					processes.AddRange(processesFound);
-				// found no processes with the names for supressed -> return false (no ignore)
 				else 
 					return false;
 			}
 			
-			// Check if the current window was this one
+			// find if the foreground (current window) is in the list of suppressed processes
 			var currentWindow = processes.FirstOrDefault(x => x.MainWindowHandle == currentWindowHandle) ?? null;
-			// There wil only ever be one "current window" (as in from foreground)
-			string mainModuleName = "";
 			try
 			{
-				if (currentWindow == null) return false;
-				if (currentWindow.MainModule != null) mainModuleName = currentWindow.MainModule.ModuleName;
+				if (currentWindow == null) return false; // current window was not in the suppressed processes
+				if (currentWindow.MainModule?.ModuleName != null) return true; // The foreground window has been matched with a suppressed process
 			}
 			catch (Exception e)
 			{
 				Log.Error("Unable to determine module name from current focus window", e);
 				return false;
-			}
-			if (!String.IsNullOrEmpty(mainModuleName))
-			{
-				if(Settings.Instance.SuppressHotKeyForProcesses.Any(
-					i => String.Compare(mainModuleName, i, comparisonType: StringComparison.InvariantCultureIgnoreCase) == 0))
-				{
-					// This window should ignore the hotkey
-					return true;
-				}
 			}
 			return false;
 		}
