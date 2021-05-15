1f07a347f5225e82b04db3146b0cd9f113bec88f
Marco van den Oever <marco@flyingpie.nl>
2021-05-15 14:42:46 +0200

    ToggleMode - Resize (new behavior, default), Move (old behavior)

---
+++ windows-terminal-quake/Settings/ToggleMode.cs
@@ -0,0 +1,15 @@
+﻿namespace WindowsTerminalQuake.Settings
+{
+	public enum ToggleMode
+	{
+		/// <summary>
+		/// "Move" keeps the size of the terminal constant, but moves the terminal off-screen to the top, which won't work great with vertically stacked monitors.
+		/// </summary>
+		Move,
+
+		/// <summary>
+		/// "Resize" should work on any setup, but may cause characters in the terminal to jump around after toggling.
+		/// </summary>
+		Resize
+	}
+}
