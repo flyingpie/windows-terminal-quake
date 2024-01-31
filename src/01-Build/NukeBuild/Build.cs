be74998fa51e7f071de39f37d66ee84318ac0aa7
Marco van den Oever <marco@flyingpie.nl>
2024-01-31 23:34:16 +0100

    Workspace

--- src/01-Build/NukeBuild/Build.cs
+++ src/01-Build/NukeBuild/Build.cs
@@ -1,36 +1,128 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.MSBuild;
+using Nuke.Common.Tools.NerdbankGitVersioning;
+using Serilog;
+using System.IO;
+using System.IO.Compression;
+using System.Numerics;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-class Build : NukeBuild
+[GitHubActions(
+	"continuous",
+	GitHubActionsImage.WindowsLatest,
+	On = new[] { GitHubActionsTrigger.Push },
+	InvokedTargets = new[] { nameof(PublishAll) })]
+public sealed class Build : NukeBuild
 {
-	public static int Main() => Execute<Build>(x => x.Compile);
+	public static int Main() => Execute<Build>(x => x.PublishAll);
 
 	[Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-	readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+	private readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-	Target Clean => _ => _
-		.Before(Restore)
+
+
+	[Required]
+	[GitRepository]
+	private readonly GitRepository GitRepository;
+
+	//[GitVersion]
+	//readonly GitVersion GitVersion;
+
+	//[NerdbankGitVersioning]
+	//readonly NerdbankGitVersioning NerdbankVersioning;
+
+	private AbsolutePath ArtifactsDirectory => RootDirectory / "_output" / "artifacts";
+
+	private AbsolutePath StagingDirectory => RootDirectory / "_output" / "staging";
+
+	//private AbsolutePath BuildDirectory => RootDirectory / "build";
+
+	//	AbsolutePath TestResultsDirectory => RootDirectory / "TestResults";
+
+	[Solution(GenerateProjects = true)]
+	private readonly Solution Solution;
+
+	private Target Clean => _ => _
+		.Executes(() =>
+		{
+			ArtifactsDirectory.CreateOrCleanDirectory();
+			//BuildDirectory.CreateOrCleanDirectory();
+		});
+
+	private Target Restore => _ => _
+		.DependsOn(Clean)
 		.Executes(() =>
 		{
 		});
 
-	Target Restore => _ => _
+	//private Target Compile => _ => _
+	//	.DependsOn(Restore)
+	//	.Executes(() =>
+	//	{
+	//		DotNetBuild(_ => _
+	//			.SetBinaryLog("msbuild.binlog")
+	//			.SetConfiguration(Configuration)
+	//			.SetFramework("net8.0-windows")
+	//			.ProjectFile(Solution._0_Host.Wtq_Windows)
+	//			.SetOutput(ArtifactsDirectory / "net8.0-windows_framework-dependent")
+	//			.SetPublishSingleFile(true)
+	//			.SetRuntime("win-x64")
+	//			.SetPublishReadyToRun(true)
+	//			.SetSelfContained(false));
+	//	});
+
+	private Target PublishNet8Win64FrameworkDependent => _ => _
+		.DependsOn(Clean)
 		.Executes(() =>
 		{
+			//Log.Information("NerdbankVersioning = {Value}", NerdbankVersioning.SimpleVersion);
+
+			var st = StagingDirectory / "net8.0-win_framework-dependent";
+			var pub = ArtifactsDirectory / "net8.0-win_framework-dependent.zip";
+
+//			var dir = ArtifactsDirectory / "net8.0-win_framework-dependent";
+//			var pub = ArtifactsDirectory / "pub";
+
+			DotNetPublish(_ => _
+				//.SetBinaryLog("msbuild.binlog")
+				.SetConfiguration(Configuration)
+				.SetFramework("net8.0-windows")
+				.SetProject(Solution._0_Host.Wtq_Windows)
+				.SetOutput(st)
+				.SetPublishSingleFile(true)
+				.SetRuntime("win-x64")
+				.SetSelfContained(false));
+
+			st.ZipTo(
+				pub,
+				filter: x => x.HasExtension(".exe", ".jsonc"),
+				compressionLevel: CompressionLevel.SmallestSize,
+				fileMode: FileMode.CreateNew);
 		});
 
-	Target Compile => _ => _
+	private Target PublishNet8Win64SelfContained => _ => _
 		.DependsOn(Restore)
+		.Executes(() =>
+		{
+			DotNetPublish(_ => _
+				.SetConfiguration(Configuration)
+				.SetFramework("net8.0-windows")
+				.SetProject(Solution._0_Host.Wtq_Windows)
+				.SetOutput(ArtifactsDirectory / "net8.0-win_self-contained")
+				.SetPublishSingleFile(true)
+				.SetRuntime("win-x64")
+				.SetSelfContained(true));
+		});
+
+	private Target PublishAll => _ => _
+		.DependsOn(PublishNet8Win64FrameworkDependent)
+		//.DependsOn(PublishNet8Win64SelfContained)
 		.Executes(() =>
 		{
 		});
