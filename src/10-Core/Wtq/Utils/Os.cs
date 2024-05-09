8ad19a824ec4236306a1da7f1c6ae6213262db69
Marco van den Oever <marco@flyingpie.nl>
2024-05-09 13:44:15 +0200

    OS extensions

---
+++ src/10-Core/Wtq/Utils/Os.cs
@@ -0,0 +1,29 @@
+﻿namespace Wtq.Utils;
+
+public static class Os
+{
+	public static bool ExistsOnPath(string fileName)
+	{
+		return GetFullPath(fileName) != null;
+	}
+
+	public static string? GetFullPath(string fileName)
+	{
+		if (File.Exists(fileName))
+		{
+			return Path.GetFullPath(fileName);
+		}
+
+		var values = Environment.GetEnvironmentVariable("PATH") ?? string.Empty;
+		foreach (var path in values.Split(Path.PathSeparator))
+		{
+			var fullPath = Path.Combine(path, fileName);
+			if (File.Exists(fullPath))
+			{
+				return fullPath;
+			}
+		}
+
+		return null;
+	}
+}
