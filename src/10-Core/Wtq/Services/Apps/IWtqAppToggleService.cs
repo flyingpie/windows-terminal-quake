a5baa33362f6061ab5da572f68ef4031aa31c767
Marco van den Oever <marco@flyingpie.nl>
2024-05-16 21:24:41 +0200

    Workspace

--- src/10-Core/Wtq/Services/Apps/IWtqAppToggleService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Wtq.Services.Apps;
-
-public interface IWtqAppToggleService
-{
-	Task ToggleOnAsync(WtqApp app, ToggleModifiers mods);
-
-	Task ToggleOffAsync(WtqApp app, ToggleModifiers mods);
-}
--- src/10-Core/Wtq/Services/WtqAppToggleService.cs
+++ src/10-Core/Wtq/Services/WtqAppToggleService.cs
@@ -1,60 +1,100 @@
 ﻿using Wtq.Data;
 
-namespace Wtq.Services.Apps;
+namespace Wtq.Services;
 
+/// <inheritdoc cref="IWtqAppToggleService"/>
 public class WtqAppToggleService(
 	IOptionsMonitor<WtqOptions> opts,
 	IWtqTween tween,
 	IWtqScreenInfoProvider scrInfoProvider)
 	: IWtqAppToggleService
 {
-	private readonly IOptionsMonitor<WtqOptions> _opts = Guard.Against.Null(opts);
-	private readonly IWtqTween _tween = Guard.Against.Null(tween);
 	private readonly ILogger _log = Log.For<WtqAppToggleService>();
+	private readonly IOptionsMonitor<WtqOptions> _opts = Guard.Against.Null(opts);
 	private readonly IWtqScreenInfoProvider _scrInfoProvider = Guard.Against.Null(scrInfoProvider);
+	private readonly IWtqTween _tween = Guard.Against.Null(tween);
 
+	/// <inheritdoc/>
+	public async Task ToggleOffAsync(WtqApp app, ToggleModifiers mods)
+	{
+		Guard.Against.Null(app);
+
+		var durationMs = GetDurationMs(app, mods);
+		var from = app.GetWindowRect();
+		var to = GetToggleOffToWindowRect(from);
+
+		_log.LogInformation("ToggleOff from '{From}' to '{To}'", from, to);
+
+		await _tween.AnimateAsync(from, to, durationMs, AnimationType.EaseInQuart, app.MoveWindow).ConfigureAwait(false);
+	}
+
+	/// <inheritdoc/>
 	public async Task ToggleOnAsync(WtqApp app, ToggleModifiers mods)
 	{
 		Guard.Against.Null(app);
 
+		// Make sure the app has focus.
 		app.BringToForeground();
 
-		var durationMs = GetDurationMs(mods);
-		var screen = GetTargetScreenBounds(app);
-
-		var to = GetToggleOnRect(app, screen);
-		var from = to with
-		{
-			Y = -to.Height - 100f,
-		};
+		var durationMs = GetDurationMs(app, mods);
+		var screen = GetToggleOnToScreenRect(app);
+		var to = GetToggleOnToWindowRect(app, screen);
+		var from = GetToggleOnFromWindowRect(to);
 
 		await _tween.AnimateAsync(from, to, durationMs, AnimationType.EaseOutQuart, app.MoveWindow).ConfigureAwait(false);
 
 		_log.LogInformation("ToggleOn from '{From}' to '{To}'", from, to);
 	}
 
-	public async Task ToggleOffAsync(WtqApp app, ToggleModifiers mods)
+	/// <summary>
+	/// Returns the time the animation should take in milliseconds.
+	/// </summary>
+	private int GetDurationMs(WtqApp app, ToggleModifiers mods)
 	{
-		Guard.Against.Null(app);
+		switch (mods)
+		{
+			case ToggleModifiers.Instant:
+				return 0;
 
-		var durationMs = GetDurationMs(mods);
+			case ToggleModifiers.SwitchingApps:
+				return 50;
 
-		var from1 = app.GetWindowRect();
+			case ToggleModifiers.None:
+			default:
+				return 250;
+		}
+	}
+
+	/// <summary>
+	/// Get the position rect a window should move to when toggling OFF.
+	/// </summary>
+	private WtqRect GetToggleOffToWindowRect(WtqRect from1)
+	{
 		var to = from1 with
 		{
 			Y = -from1.Height - 100f,
 		};
 
-		_log.LogInformation("ToggleOn from '{From}' to '{To}'", from1, to);
+		return to;
+	}
 
-		await _tween.AnimateAsync(from1, to, durationMs, AnimationType.EaseInQuart, app.MoveWindow).ConfigureAwait(false);
+	/// <summary>
+	/// Get the position rect a window should move to when toggling ON.
+	/// </summary>
+	private WtqRect GetToggleOnFromWindowRect(WtqRect to)
+	{
+		var from = to with
+		{
+			Y = -to.Height - 100f,
+		};
+		return from;
 	}
 
 	/// <summary>
 	/// Returns a rectangle representing the screen we want to toggle the terminal onto.
 	/// </summary>
 	/// <param name="app">The app for which we're figuring out the screen bounds.</param>
-	public WtqRect GetTargetScreenBounds(WtqApp app)
+	private WtqRect GetToggleOnToScreenRect(WtqApp app)
 	{
 		Guard.Against.Null(app);
 
@@ -65,18 +105,18 @@ public class WtqAppToggleService(
 		{
 			case PreferMonitor.AtIndex:
 			{
-				var scrs = _scrInfoProvider.GetScreenRects();
+				var screens = _scrInfoProvider.GetScreenRects();
 
-				if (scrs.Length > monInd)
+				if (screens.Length > monInd)
 				{
-					return scrs[monInd];
+					return screens[monInd];
 				}
 
 				_log.LogWarning(
 					"Option '{OptionName}' was set to {MonitorIndex}, but only {MonitorCount} screens were found, falling back to primary",
 					nameof(app.Options.MonitorIndex),
 					monInd,
-					scrs.Length);
+					screens.Length);
 
 				return _scrInfoProvider.GetPrimaryScreenRect();
 			}
@@ -88,27 +128,25 @@ public class WtqAppToggleService(
 				return _scrInfoProvider.GetScreenWithCursor();
 
 			default:
+			{
 				_log.LogWarning(
-					"Unknown value '{OptionValue}' for option '{OptionName}'",
+					"Unknown value '{OptionValue}' for option '{OptionName}', falling back to primary",
 					prefMon,
 					nameof(app.Options.PreferMonitor));
+
 				return _scrInfoProvider.GetPrimaryScreenRect();
+			}
 		}
 	}
 
 	/// <summary>
 	/// Returns the target bounds of the specified <param name="app"/>, when its toggling onto the screen.
 	/// </summary>
-	/// <param name="app">The app that's being toggle on. Used to fetch its options.</param>
-	/// <param name="screenBounds">The screen onto which the app is being toggled, used for alignment.</param>
-	/// <param name="progress">How far along we are in the animation, '0' for not started, to '1' for finished.</param>
-	public WtqRect GetToggleOnRect(
+	private WtqRect GetToggleOnToWindowRect(
 		WtqApp app,
 		WtqRect screenBounds)
 	{
 		Guard.Against.Null(app);
-
-		// TODO: Version that moves apps off the bottom?
 
 		// Calculate terminal size.
 		var termWidth = (int)(screenBounds.Width * _opts.CurrentValue.HorizontalScreenCoverageIndexForApp(app.Options));
@@ -141,21 +179,5 @@ public class WtqAppToggleService(
 			// Vertical Height, based on the VerticalScreenCoverage, VerticalOffset, and current progress of the animation
 			Height = termHeight,
 		};
-	}
-
-	private static int GetDurationMs(ToggleModifiers mods)
-	{
-		switch (mods)
-		{
-			case ToggleModifiers.Instant:
-				return 0;
-
-			case ToggleModifiers.SwitchingApps:
-				return 50;
-
-			case ToggleModifiers.None:
-			default:
-				return 250;
-		}
 	}
 }
