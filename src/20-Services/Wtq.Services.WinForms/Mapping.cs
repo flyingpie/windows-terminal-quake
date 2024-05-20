e9ba5e3ecf06ff9280ff191e141ed21d83394529
Marco van den Oever <marco@flyingpie.nl>
2024-05-20 23:20:15 +0200

    Restored WinForms hot key manager, SharpHook is giving too many issues atm

---
+++ src/20-Services/Wtq.Services.WinForms/Mapping.cs
@@ -0,0 +1,14 @@
+﻿namespace Wtq.Services.WinForms;
+
+internal static class Mapping
+{
+	internal static Configuration.Keys ToWtqKeys(this System.Windows.Forms.Keys key)
+	{
+		return (Configuration.Keys)key;
+	}
+
+	internal static Configuration.KeyModifiers ToWtqKeyModifiers(this Native.KeyModifiers mask)
+	{
+		return (Configuration.KeyModifiers)mask;
+	}
+}
