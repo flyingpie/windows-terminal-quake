ea248b5b882a3c1fe3544d079b14ed78c73113d6
Marco van den Oever <marco@flyingpie.nl>
2024-07-14 14:14:32 +0200

    [Services.Win32] Added global using

--- src/20-Services/Wtq.Services.Win32/GlobalUsings.cs
+++ src/20-Services/Wtq.Services.Win32/GlobalUsings.cs
@@ -6,4 +6,5 @@ global using System.ComponentModel;
 global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Linq;
+global using System.Threading;
 global using System.Threading.Tasks;
