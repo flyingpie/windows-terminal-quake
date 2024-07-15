6a1141cf4af27f61b4cd2eaa6eaa8d56862c2cbc
Marco van den Oever <marco@flyingpie.nl>
2024-07-15 15:30:14 +0200

    GUI

--- src/30-Host/Wtq.Host.Windows/Program.cs
+++ src/30-Host/Wtq.Host.Windows/Program.cs
@@ -2,10 +2,17 @@
 
 public static class Program
 {
+	[STAThread]
 	public static async Task Main(string[] args)
 	{
 		Utils.Log.Configure();
 
-		await new WtqWin32().RunAsync().ConfigureAwait(false);
+		var app = new WtqWin32().RunAsync().ConfigureAwait(false);
+
+		var ui = new WtqUI.WtqUI();
+
+		ui.Start(args);
+
+		await app;
 	}
 }
