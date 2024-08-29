f8644a028cafcf143cfc27bcf3e76a174ee80a4a
Marco van den Oever <marco@flyingpie.nl>
2024-08-30 00:02:33 +0200

    Workspace

--- src/20-Services/Wtq.Services.KWin/DBus/DBusConnection.cs
+++ src/20-Services/Wtq.Services.KWin/DBus/DBusConnection.cs
@@ -1,39 +1,74 @@
-using Microsoft.Extensions.Hosting;
 using Tmds.DBus;
+using Wtq.Exceptions;
 using Address = Tmds.DBus.Protocol.Address;
 using Connection = Tmds.DBus.Protocol.Connection;
 
 namespace Wtq.Services.KWin.DBus;
 
-internal class DBusConnection : IDBusConnection
+/// <inheritdoc/>
+public class DBusConnection : IDBusConnection
 {
+	private readonly ILogger _log = Log.For<DBusConnection>();
+
 	public DBusConnection()
 	{
-		ClientConnection = new Connection(Address.Session);
-		ServerConnection = new Tmds.DBus.Connection(Address.Session);
+		var address = Address.Session;
+
+		_log.LogInformation("Setting up DBus using address {Address}", address);
+
+		if (string.IsNullOrWhiteSpace(address))
+		{
+			throw new WtqException("Could not determine address for session DBus.");
+		}
+
+		ClientConnection = new Connection(address);
+		ServerConnection = new Tmds.DBus.Connection(address);
 	}
 
+	/// <inheritdoc/>
 	public Connection ClientConnection { get; }
 
+	/// <inheritdoc/>
 	public Tmds.DBus.Connection ServerConnection { get; }
 
+	/// <summary>
+	/// Connects to DBus.
+	/// </summary>
 	public async Task StartAsync(CancellationToken cancellationToken)
 	{
-		await ClientConnection.ConnectAsync().ConfigureAwait(false);
-		await ServerConnection.ConnectAsync().ConfigureAwait(false);
+		_log.LogInformation("Setting up DBus connections");
+
+		var sw = Stopwatch.StartNew();
+		await ClientConnection.ConnectAsync().NoCtx();
+		_log.LogInformation("DBus client connection ready, took {Elapsed}", sw.Elapsed);
+
+		sw.Restart();
+		await ServerConnection.ConnectAsync().NoCtx();
+		_log.LogInformation("DBus server connection ready, took {Elapsed}", sw.Elapsed);
 	}
 
+	/// <summary>
+	/// Cleans up connections to DBus.
+	/// </summary>
 	public Task StopAsync(CancellationToken cancellationToken)
 	{
+		_log.LogInformation("Cleaning up DBus connections");
+
 		ClientConnection.Dispose();
 		ServerConnection.Dispose();
 
 		return Task.CompletedTask;
 	}
 
+	/// <inheritdoc/>
 	public async Task RegisterServiceAsync(string serviceName, IDBusObject serviceObject)
 	{
-		await ServerConnection.RegisterServiceAsync(serviceName).ConfigureAwait(false);
-		await ServerConnection.RegisterObjectAsync(serviceObject).ConfigureAwait(false);
+		Guard.Against.NullOrWhiteSpace(serviceName);
+		Guard.Against.Null(serviceObject);
+
+		_log.LogInformation("Registering DBus service with name '{ServiceName}', and object '{ServiceObject}'", serviceName, serviceObject);
+
+		await ServerConnection.RegisterServiceAsync(serviceName).NoCtx();
+		await ServerConnection.RegisterObjectAsync(serviceObject).NoCtx();
 	}
 }
--- src/20-Services/Wtq.Services.KWin/DBus/IDBusConnection.cs
+++ src/20-Services/Wtq.Services.KWin/DBus/IDBusConnection.cs
@@ -3,11 +3,24 @@ using Connection = Tmds.DBus.Protocol.Connection;
 
 namespace Wtq.Services.KWin.DBus;
 
+/// <summary>
+/// Wraps both a client- and a server connection to DBus.<br/>
+/// The client connection is used for sending requests to DBus, the server one is used to register DBus object.
+/// </summary>
 public interface IDBusConnection
 {
-	public Connection ClientConnection { get; }
+	/// <summary>
+	/// Client connection, used to send requests to DBus.
+	/// </summary>
+	Connection ClientConnection { get; }
 
-	public Tmds.DBus.Connection ServerConnection { get; }
+	/// <summary>
+	/// Server connection, used to register DBus objects.
+	/// </summary>
+	Tmds.DBus.Connection ServerConnection { get; }
 
+	/// <summary>
+	/// Register an object that exposes methods that can be called by other processes.
+	/// </summary>
 	Task RegisterServiceAsync(string serviceName, IDBusObject serviceObject);
 }
