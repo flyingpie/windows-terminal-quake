d1938c7c0a1a6d0c8823358bc4b0340d5df53cfb
Marco van den Oever <marco@flyingpie.nl>
2024-09-03 00:54:11 +0200

    Moved non-platform-specific host stuff to WtqHostBase

---
+++ src/10-Core/Wtq/WtqHostBase.cs
@@ -0,0 +1,81 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Wtq.Configuration;
+using Wtq.Events;
+using Wtq.Services;
+using Wtq.Utils;
+
+namespace Wtq;
+
+public class WtqHostBase
+{
+	private readonly IHost _host;
+
+	public WtqHostBase()
+	{
+		var log = Utils.Log.For(typeof(WtqHostBase));
+
+		// Configuration.
+		var pathToWtqConf = WtqOptionsPath.Instance.Path;
+		var config = new ConfigurationBuilder()
+			.SetBasePath(Path.GetDirectoryName(pathToWtqConf)!)
+			.AddJsonFile(f =>
+			{
+				f.ReloadOnChange = true;
+				f.Optional = false;
+				f.Path = Path.GetFileName(pathToWtqConf);
+				f.OnLoadException = x =>
+				{
+					log.LogError(x.Exception, "Error loading configuration file '{File}': {Message}", pathToWtqConf, x.Exception.Message);
+					Console.WriteLine($"Error loading configuration file '{pathToWtqConf}': {x.Exception.Message}");
+
+					// MessageBox.Show($"Error loading configuration file '{pathToWtqConf}': {x.Exception.Message}");
+				};
+			})
+			.Build();
+
+		_host = new HostBuilder()
+			.ConfigureAppConfiguration(opt =>
+			{
+				opt.AddConfiguration(config);
+			})
+			.ConfigureServices(opt =>
+			{
+				opt
+					.AddOptionsWithValidateOnStart<WtqOptions>()
+					.Bind(config);
+
+				opt
+
+					// Utils
+					.AddWtqCore();
+
+				ConfigureServices(opt);
+			})
+			.UseSerilog()
+			.Build();
+	}
+
+	public async Task RunAsync()
+	{
+		try
+		{
+			await _host
+				.RunAsync()
+				.ConfigureAwait(false);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error running application: {ex}");
+
+			// MessageBox.Show($"Error running application: {ex}", "Error starting WTQ");
+		}
+	}
+
+	protected virtual void ConfigureServices(IServiceCollection services)
+	{
+	}
+}
