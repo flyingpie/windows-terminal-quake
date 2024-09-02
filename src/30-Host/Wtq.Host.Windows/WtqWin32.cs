63469e58ef556cf45c011857006ff1fbbb487da6
Marco van den Oever <marco@flyingpie.nl>
2024-09-03 00:54:11 +0200

    Moved non-platform-specific host stuff to WtqHostBase

--- src/30-Host/Wtq.Host.Windows/WtqWin32.cs
+++ src/30-Host/Wtq.Host.Windows/WtqWin32.cs
@@ -1,76 +1,19 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Serilog;
-using Wtq.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Wtq.Services.Win32;
 using Wtq.Services.WinForms;
 
 namespace Wtq.Host.Windows;
 
-public class WtqWin32
+public class WtqWin32 : WtqHostBase
 {
-	private readonly IHost _host;
-
-	public WtqWin32()
+	protected override void ConfigureServices(IServiceCollection services)
 	{
-		var log = Utils.Log.For(typeof(Program));
+		Guard.Against.Null(services);
 
-		// Configuration.
-		var pathToWtqConf = WtqOptionsPath.Instance.Path;
-		var config = new ConfigurationBuilder()
-			.SetBasePath(Path.GetDirectoryName(pathToWtqConf)!)
-			.AddJsonFile(f =>
-			{
-				f.ReloadOnChange = true;
-				f.Optional = false;
-				f.Path = Path.GetFileName(pathToWtqConf);
-				f.OnLoadException = x =>
-				{
-					log.LogError(x.Exception, "Error loading configuration file '{File}': {Message}", pathToWtqConf, x.Exception.Message);
-					Console.WriteLine($"Error loading configuration file '{pathToWtqConf}': {x.Exception.Message}");
-					MessageBox.Show($"Error loading configuration file '{pathToWtqConf}': {x.Exception.Message}");
-				};
-			})
-			.Build();
-
-		_host = new HostBuilder()
-			.ConfigureAppConfiguration(opt =>
-			{
-				opt.AddConfiguration(config);
-			})
-			.ConfigureServices(opt =>
-			{
-				opt
-					.AddOptionsWithValidateOnStart<WtqOptions>()
-					.Bind(config);
-
-				opt
-					.AddWtqCore()
-
-					// Platform-specific.
-					.AddWin32ProcessService()
-					.AddWinFormsHotKeyService()
-					.AddWinFormsScreenInfoProvider()
-					.AddWinFormsTrayIcon();
-			})
-			.UseSerilog()
-			.Build();
-	}
-
-	public async Task RunAsync()
-	{
-		try
-		{
-			await _host
-				.RunAsync()
-				.ConfigureAwait(false);
-		}
-		catch (Exception ex)
-		{
-			Console.WriteLine($"Error running application: {ex}");
-			MessageBox.Show($"Error running application: {ex}", "Error starting WTQ");
-		}
+		services
+			.AddWin32ProcessService()
+			.AddWinFormsHotKeyService()
+			.AddWinFormsScreenInfoProvider()
+			.AddWinFormsTrayIcon();
 	}
 }
