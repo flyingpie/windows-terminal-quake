380301fde1b515f2fb1e23968892970f99c7ba85
Marco van den Oever <marco@flyingpie.nl>
2024-10-05 13:34:34 +0200

    [Win32ProcessService] Build fix

--- src/20-Services/Wtq.Services.Win32/Win32ProcessService.cs
+++ src/20-Services/Wtq.Services.Win32/Win32ProcessService.cs
@@ -17,7 +17,7 @@ public sealed class Win32ProcessService :
 	private readonly SemaphoreSlim _lock = new(1);
 
 	private DateTimeOffset _nextLookup = DateTimeOffset.MinValue;
-	private IEnumerable<WtqWindow> _processes = [];
+	private ICollection<WtqWindow> _processes = [];
 
 	public async Task CreateAsync(WtqAppOptions opts)
 	{
@@ -35,7 +35,7 @@ public sealed class Win32ProcessService :
 	{
 		Guard.Against.Null(opts);
 
-		var processes = await GetProcessesAsync().NoCtx();
+		var processes = await GetWindowsAsync().NoCtx();
 
 		return processes.FirstOrDefault(p => p.Matches(opts));
 	}
@@ -56,6 +56,13 @@ public sealed class Win32ProcessService :
 		}
 
 		return null;
+	}
+
+	public async Task<ICollection<WtqWindow>> GetWindowsAsync()
+	{
+		await UpdateProcessesAsync().NoCtx();
+
+		return _processes;
 	}
 
 	public async Task StartAsync(CancellationToken cancellationToken)
@@ -114,13 +121,6 @@ public sealed class Win32ProcessService :
 				});
 
 		await UpdateProcessesAsync(force: true).NoCtx();
-	}
-
-	private async Task<IEnumerable<WtqWindow>> GetProcessesAsync()
-	{
-		await UpdateProcessesAsync().NoCtx();
-
-		return _processes;
 	}
 
 	private async Task UpdateProcessesAsync(bool force = false)
