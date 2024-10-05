d523bad0cb1dbf07f57a43fa803143771ae521a3
Marco van den Oever <marco@flyingpie.nl>
2024-10-05 12:33:44 +0200

    [KWinHotKeyService] Some cleanup

--- src/20-Services/Wtq.Services.KWin/KWinHotKeyService.cs
+++ src/20-Services/Wtq.Services.KWin/KWinHotKeyService.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Tmds.DBus.Protocol;
 using Wtq.Configuration;
 using Wtq.Services.KWin.DBus;
 
@@ -28,24 +29,45 @@ internal class KWinHotKeyService : IDisposable, IHostedService
 		// Nothing to do.
 	}
 
+	private IDisposable _disp1;
+	private IDisposable _disp2;
+
 	public async Task StartAsync(CancellationToken cancellationToken)
 	{
 		var gl = _kwinService.CreateKGlobalAccel("/kglobalaccel");
 		var comp = _kwinService.CreateComponent("/component/kwin");
 		var kwin = _kwinService.CreateKWin("/org/kde/KWin");
 
-		// TODO(MvdO): Reset any previous shortcuts first, then re-register new ones.
-		// Should also be done when configuration changes.
-		// Also run on app stop, although that's less reliable.
-		var names = await comp.ShortcutNamesAsync();
-		var inf = await comp.AllShortcutInfosAsync();
-		var x = await gl.AllComponentsAsync();
+		// Clear.
+		for (int i = 0; i < 5; i++)
+		{
+			var resx1 = await gl.UnregisterAsync("kwin", $"wtq_hk1_00{i + 1}_scr_text");
+		}
+
+		await comp.CleanUpAsync();
+
+		// TODO: Although we haven't gotten shortcut registration to work reliably through direct DBus calls,
+		// we _can_ catch when shortcuts are being pressed/released.
+		// So dial down the JS part to just registration, remove the callback to WTQ part.
+		_disp1 = await comp.WatchGlobalShortcutPressedAsync((exception, tuple) =>
+		{
+			var xx2 = 2;
+			Console.WriteLine($"PRESSED:{tuple.ComponentUnique} {tuple.ShortcutUnique} {tuple.Timestamp} {exception?.Message}");
+		});
+
+		_disp2 = await comp.WatchGlobalShortcutReleasedAsync((exception, tuple) =>
+		{
+			Console.WriteLine($"RELEASED:{tuple.ComponentUnique} {tuple.ShortcutUnique} {tuple.Timestamp} {exception?.Message}");
+		});
+
+		await _scriptExecutor.RegisterHotkeyAsync("wtq_hk1_005_scr", KeyModifiers.Control, Keys.Q);
 
 		await _scriptExecutor.RegisterHotkeyAsync("wtq_hk1_001_scr", KeyModifiers.Control, Keys.D1);
 		await _scriptExecutor.RegisterHotkeyAsync("wtq_hk1_002_scr", KeyModifiers.Control, Keys.D2);
 		await _scriptExecutor.RegisterHotkeyAsync("wtq_hk1_003_scr", KeyModifiers.Control, Keys.D3);
 		await _scriptExecutor.RegisterHotkeyAsync("wtq_hk1_004_scr", KeyModifiers.Control, Keys.D4);
-		await _scriptExecutor.RegisterHotkeyAsync("wtq_hk1_005_scr", KeyModifiers.Control, Keys.Q);
+		await _scriptExecutor.RegisterHotkeyAsync("wtq_hk1_006_scr", KeyModifiers.Control, Keys.D5);
+		await _scriptExecutor.RegisterHotkeyAsync("wtq_hk1_007_scr", KeyModifiers.Control, Keys.D6);
 	}
 
 	public Task StopAsync(CancellationToken cancellationToken)
