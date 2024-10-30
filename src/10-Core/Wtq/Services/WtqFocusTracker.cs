b4a4eba12be47adbe39275328a153178da4ec521
Marco van den Oever <marco@flyingpie.nl>
2024-10-30 23:12:38 +0100

    [WtqFocusTracker] Simplified focus tracker

--- src/10-Core/Wtq/Services/WtqFocusTracker.cs
+++ src/10-Core/Wtq/Services/WtqFocusTracker.cs
@@ -1,107 +1,67 @@
-using Microsoft.Extensions.Hosting;
-using Wtq.Events;
-
 namespace Wtq.Services;
 
+/// <summary>
+/// Runs in the background, keeping track of what window has focus.<br/>
+/// Fires <see cref="WtqWindowFocusChangedEvent"/> events when focus changes from window to the next.
+/// </summary>
 public sealed class WtqFocusTracker(
-	IWtqAppRepo appsRepo,
 	IWtqBus bus,
-	IWtqProcessService procService)
-	: IHostedService, IWtqFocusTracker
+	IWtqWindowService windowService)
+	: IAsyncInitializable, IAsyncDisposable
 {
-	private readonly IWtqAppRepo _appsRepo = Guard.Against.Null(appsRepo);
 	private readonly IWtqBus _bus = Guard.Against.Null(bus);
 	private readonly ILogger _log = Log.For<WtqFocusTracker>();
-	private readonly IWtqProcessService _procService = Guard.Against.Null(procService);
+	private readonly IWtqWindowService _windowService = Guard.Against.Null(windowService);
 
-	private WtqApp? _foregroundApp;
 	private bool _isRunning = true;
-	private WtqWindow? _lastNonWtqForeground;
 
-	/// <inheritdoc/>
-	public async Task FocusLastNonWtqAppAsync()
-	{
-		await (_lastNonWtqForeground?.BringToForegroundAsync() ?? Task.CompletedTask).ConfigureAwait(false);
-	}
+	private WtqWindow? _prev;
 
-	[SuppressMessage("Reliability", "CA2016:Forward the 'CancellationToken' parameter to methods", Justification = "MvdO: We do not want the task to be cancelled here.")]
-	public Task StartAsync(CancellationToken cancellationToken)
+	public Task InitializeAsync()
 	{
-		_ = Task.Run(async () =>
-		{
-			while (_isRunning)
+		// TODO: Generalize loop.
+		_ = Task.Run(
+			async () =>
 			{
-				try
+				while (_isRunning)
 				{
-					await Task.Delay(TimeSpan.FromMilliseconds(250)).ConfigureAwait(false);
-
-					var fgWindow = _procService.GetForegroundWindow();
-
-					var pr = _appsRepo.Apps.FirstOrDefault(a => a.Process == fgWindow);
-
-					if (pr == null)
+					try
 					{
-						_lastNonWtqForeground = fgWindow;
-					}
+						// Get current foreground window (could be null).
+						var curr = await _windowService.GetForegroundWindowAsync().NoCtx();
 
-					// Nothing changed.
-					if (pr == _foregroundApp)
-					{
-						continue;
-					}
-
-					// Did not have focus before, got focus now.
-					if (_foregroundApp == null && pr != null)
-					{
-						// App gained focus.
-						_log.LogInformation("App '{App}' gained focus", pr);
-						_foregroundApp = pr;
-						_bus.Publish(new WtqAppFocusEvent()
+						// If the window that has focus now, is not the one that had focus last cycle, focus has changed.
+						// Note that both the past- and the future window can be null.
+						if (_prev != curr)
 						{
-							App = pr,
-							GainedFocus = true,
-						});
-						continue;
-					}
+							_log.LogInformation("Focus went from window '{LostFocus}' to window {GotFocus})", _prev, curr);
 
-					if (_foregroundApp == null || _foregroundApp == pr)
+							_bus.Publish(new WtqWindowFocusChangedEvent()
+							{
+								GotFocusWindow = curr,
+								LostFocusWindow = _prev,
+							});
+						}
+
+						// Store for next cycle.
+						_prev = curr;
+					}
+					catch (Exception ex)
 					{
-						continue;
+						_log.LogError(ex, "Error tracking focus: {Message}", ex.Message);
 					}
 
-					if (pr == null)
-					{
-						// App lost focus.
-						_log.LogInformation("App '{App}' lost focus (went to window {Window})", _foregroundApp, fgWindow);
-
-						_bus.Publish(new WtqAppFocusEvent()
-						{
-							App = _foregroundApp,
-							GainedFocus = false,
-						});
-
-						_foregroundApp = null;
-					}
-					else
-					{
-						_log.LogInformation("Focus moved from app '{AppFrom}' to app '{AppTo}'", _foregroundApp, pr);
-						_foregroundApp = pr;
-					}
+					await Task.Delay(TimeSpan.FromMilliseconds(250)).NoCtx();
 				}
-				catch (Exception ex)
-				{
-					_log.LogError(ex, "Error tracking focus: {Message}", ex.Message);
-				}
-			}
-		});
+			});
 
 		return Task.CompletedTask;
 	}
 
-	public Task StopAsync(CancellationToken cancellationToken)
+	public ValueTask DisposeAsync()
 	{
 		_isRunning = false;
 
-		return Task.CompletedTask;
+		return ValueTask.CompletedTask;
 	}
 }
