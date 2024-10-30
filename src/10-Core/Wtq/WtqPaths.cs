fb4817b6e0b4e31e7258418afe04792168ef5dc9
Marco van den Oever <marco@flyingpie.nl>
2024-10-30 22:49:31 +0100

    [WtqPaths] Added XDG .config folder

--- src/10-Core/Wtq/WtqPaths.cs
+++ src/10-Core/Wtq/WtqPaths.cs
@@ -39,11 +39,15 @@ public static class WtqPaths
 	/// </summary>
 	public static IEnumerable<string> WtqConfigPaths { get; } =
 	[
-
 		// Next to wtq executable.
 		Path.Combine(GetWtqAppDir(), "wtq.json"),
 		Path.Combine(GetWtqAppDir(), "wtq.jsonc"),
 		Path.Combine(GetWtqAppDir(), "wtq.json5"),
+
+		// In XDG config dir.
+		Path.Combine(UserHome, ".config", "wtq.json"),
+		Path.Combine(UserHome, ".config", "wtq.jsonc"),
+		Path.Combine(UserHome, ".config", "wtq.json5"),
 
 		// In user home dir.
 		Path.Combine(UserHome, "wtq.json"),
