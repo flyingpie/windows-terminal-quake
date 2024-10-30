9b862c858b3c22f64d36913d84083725fcb80780
Marco van den Oever <marco@flyingpie.nl>
2024-10-30 23:08:02 +0100

    [WtqAppRepo] Added some new ways of fetching apps

--- src/10-Core/Wtq/Services/WtqAppRepo.cs
+++ src/10-Core/Wtq/Services/WtqAppRepo.cs
@@ -1,56 +1,125 @@
-using Microsoft.Extensions.Hosting;
-
 namespace Wtq.Services;
 
-public sealed class WtqAppRepo : IHostedService, IWtqAppRepo
+/// <inheritdoc cref="IWtqAppRepo"/>.
+public sealed class WtqAppRepo : IWtqAppRepo
 {
 	private readonly ILogger _log = Log.For<WtqAppRepo>();
 	private readonly IOptionsMonitor<WtqOptions> _opts;
-	private readonly IWtqProcessFactory _procFactory;
-	private readonly IWtqProcessService _procService;
-	private readonly IWtqScreenInfoProvider _screenInfoProvider;
 	private readonly IWtqAppToggleService _toggleService;
+	private readonly IWtqScreenInfoProvider _screenInfoProvider;
+	private readonly IWtqWindowResolver _windowResolver;
 
 	private readonly List<WtqApp> _apps = [];
 
 	public WtqAppRepo(
 		IOptionsMonitor<WtqOptions> opts,
-		IWtqProcessFactory procFactory,
-		IWtqProcessService procService,
+		IWtqAppToggleService toggleService,
 		IWtqScreenInfoProvider screenInfoProvider,
-		IWtqAppToggleService toggleService)
+		IWtqWindowResolver procResolver)
 	{
 		_opts = Guard.Against.Null(opts);
-		_procFactory = Guard.Against.Null(procFactory);
-		_procService = Guard.Against.Null(procService);
-		_screenInfoProvider = Guard.Against.Null(screenInfoProvider);
 		_toggleService = Guard.Against.Null(toggleService);
+		_screenInfoProvider = Guard.Against.Null(screenInfoProvider);
+		_windowResolver = Guard.Against.Null(procResolver);
 
-		// Whenever the settings file changes, update the list of tracked apps.
-		opts.OnChange(o => _ = Task.Run(async () => await UpdateAppsAsync().NoCtx()));
+		// Whenever the settings change, update the list of tracked apps.
+		opts.OnChange(o => _ = Task.Run(() => UpdateAppsAsync(allowStartNew: false)));
 	}
 
-	public IEnumerable<WtqApp> Apps => _apps;
-
-	public async Task UpdateAppsAsync()
+	public async Task InitializeAsync()
 	{
-		_log.LogInformation("Updating apps");
+		// TODO: Make setting for "allowStartNew"? As in, allow starting apps on WTQ first start?
+		// "StartApps": "OnWtqStart | OnHotkeyPress"
+		await UpdateAppsAsync(allowStartNew: true).NoCtx();
+	}
+
+	public async ValueTask DisposeAsync()
+	{
+		foreach (var app in _apps)
+		{
+			await app.DisposeAsync().NoCtx();
+		}
+	}
+
+	/// <inheritdoc/>
+	public WtqApp? GetByName(string name)
+	{
+		Guard.Against.NullOrWhiteSpace(name);
+
+		return _apps.Find(a => a.Name == name);
+	}
+
+	/// <inheritdoc/>
+	public WtqApp? GetByWindow(WtqWindow window)
+	{
+		Guard.Against.Null(window);
+
+		return _apps.FirstOrDefault(a => a.Window == window);
+	}
+
+	/// <inheritdoc/>
+	public WtqApp? GetOpen()
+	{
+		return _apps.FirstOrDefault(a => a.IsOpen);
+	}
+
+	/// <inheritdoc/>
+	public WtqApp? GetPrimary()
+	{
+		return _apps.FirstOrDefault();
+	}
+
+	private WtqApp Create(WtqAppOptions app)
+	{
+		Guard.Against.Null(app);
+
+		return new WtqApp(
+			_opts,
+			_windowResolver,
+			_screenInfoProvider,
+			_toggleService,
+			() => GetOptionsByNameRequired(app.Name),
+			app.Name);
+	}
+
+	private WtqAppOptions? GetOptionsByName(string name)
+	{
+		Guard.Against.NullOrWhiteSpace(name);
+
+		return _opts
+			.CurrentValue
+			.Apps
+			.FirstOrDefault(o => o.Name?.Equals(name, StringComparison.OrdinalIgnoreCase) ?? false);
+	}
+
+	private WtqAppOptions GetOptionsByNameRequired(string name)
+	{
+		return GetOptionsByName(name)
+			?? throw new WtqException($"No instance found of type '{nameof(WtqAppOptions)}' found with name '{name}'.");
+	}
+
+	private async Task UpdateAppsAsync(bool allowStartNew)
+	{
+		_log.LogDebug("Updating apps");
 
 		// Add app handles for options that don't have one yet.
 		foreach (var opt in _opts.CurrentValue.Apps)
 		{
-			var app = GetAppByName(opt.Name);
+			var app = GetByName(opt.Name);
 
-			if (app == null)
+			if (app != null)
 			{
-				_log.LogInformation("Missing app handle for {Options}, creating one now", opt);
-
-				// Create & update app handle.
-				app = Create(opt);
-				await app.UpdateProcessAsync().NoCtx();
-
-				_apps.Add(app);
+				continue;
 			}
+
+			_log.LogInformation("Missing app handle for {Options}, creating one now", opt);
+
+			// Create & update app handle.
+			app = Create(opt);
+
+			await app.UpdateLocalAppStateAsync(allowStartNew).NoCtx();
+
+			_apps.Add(app);
 		}
 
 		// Remove app handles for dropped options.
@@ -58,71 +127,16 @@ public sealed class WtqAppRepo : IHostedService, IWtqAppRepo
 		{
 			var opt = GetOptionsByName(app.Name);
 
-			if (opt == null)
+			if (opt != null)
 			{
-				_log.LogInformation("Dropped options {Options}, removing app handle {App}", opt, app);
-				await app.DisposeAsync().ConfigureAwait(false);
-
-				_apps.Remove(app);
+				continue;
 			}
+
+			_log.LogInformation("Dropped options {Options}, removing app handle {App}", opt, app);
+
+			await app.DisposeAsync().NoCtx();
+
+			_apps.Remove(app);
 		}
-	}
-
-	public WtqApp? GetAppByName(string name)
-	{
-		Guard.Against.NullOrWhiteSpace(name);
-
-		return _apps.Find(a => a.Name == name);
-	}
-
-	public WtqApp GetAppByNameRequired(string name)
-	{
-		return GetAppByName(name)
-			?? throw new WtqException($"No instance found of type '{nameof(WtqApp)}' found with name '{name}'.");
-	}
-
-	public async ValueTask DisposeAsync()
-	{
-		foreach (var app in Apps)
-		{
-			await app.DisposeAsync().ConfigureAwait(false);
-		}
-	}
-
-	public WtqApp Create(WtqAppOptions app)
-	{
-		Guard.Against.Null(app);
-
-		return new WtqApp(
-			_opts,
-			_procFactory,
-			_procService,
-			_screenInfoProvider,
-			_toggleService,
-			() => GetOptionsByNameRequired(app.Name),
-			app.Name);
-	}
-
-	public async Task StartAsync(CancellationToken cancellationToken)
-	{
-		await UpdateAppsAsync().ConfigureAwait(false);
-	}
-
-	public Task StopAsync(CancellationToken cancellationToken)
-	{
-		return Task.CompletedTask;
-	}
-
-	private WtqAppOptions? GetOptionsByName(string name)
-	{
-		Guard.Against.NullOrWhiteSpace(name);
-
-		return _opts.CurrentValue.Apps.FirstOrDefault(o => o.Name?.Equals(name, StringComparison.OrdinalIgnoreCase) ?? false);
-	}
-
-	private WtqAppOptions GetOptionsByNameRequired(string name)
-	{
-		return GetOptionsByName(name)
-			?? throw new WtqException($"No instance found of type '{nameof(WtqAppOptions)}' found with name '{name}'.");
 	}
 }
