75c623b7cf2a524402cbc51eed3fbf986fe21e03
Marco van den Oever <marco@flyingpie.nl>
2024-11-07 00:15:32 +0100

    Toggle direction (#148)

--- src/10-Core/Wtq/Configuration/WtqAppOptions.cs
+++ src/10-Core/Wtq/Configuration/WtqAppOptions.cs
@@ -60,6 +60,9 @@ public sealed class WtqAppOptions
 	/// <inheritdoc cref="WtqOptions.TaskBarIconVisibility"/>
 	public TaskBarIconVisibility? TaskbarIconVisibility { get; set; }
 
+	/// <inheritdoc cref="WtqOptions.OffScreenLocations"/>
+	public IEnumerable<OffScreenLocation>? OffScreenLocations { get; init; }
+
 	/// <inheritdoc cref="WtqOptions.VerticalOffset"/>
 	public int? VerticalOffset { get; set; }
 
