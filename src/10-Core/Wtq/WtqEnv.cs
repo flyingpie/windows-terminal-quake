047f3c831fba4ad53791550ea2f84e746c7b2ee7
Marco van den Oever <marco@flyingpie.nl>
2024-11-22 23:53:20 +0100

    Support WTQ_CONFIG_FILE environment variable, WTQ_LOG_LEVEL log level

---
+++ src/10-Core/Wtq/WtqEnv.cs
@@ -0,0 +1,27 @@
+using Serilog.Events;
+
+namespace Wtq;
+
+public static class WtqEnv
+{
+	public static class Names
+	{
+		public const string Config = "WTQ_CONFIG_FILE";
+
+		public const string LogLevel = "WTQ_LOG_LEVEL";
+	}
+
+	/// <summary>
+	/// Returns path to the WTQ config file, as specified by an environment variable.
+	/// </summary>
+	public static string ConfigFile
+		=> Environment.GetEnvironmentVariable(Names.Config) ?? string.Empty;
+
+	/// <summary>
+	/// Returns the requested log level, as specified by an environment variable.
+	/// </summary>
+	public static LogEventLevel LogLevel
+		=> Enum.TryParse<LogEventLevel>(Names.LogLevel, ignoreCase: true, out var res)
+		? res
+		: LogEventLevel.Information;
+}
