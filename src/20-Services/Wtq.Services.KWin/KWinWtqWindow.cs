fa63122d1a3deb2f1d06bccd32062cf6bcc073fe
Marco van den Oever <marco@flyingpie.nl>
2024-11-23 14:14:22 +0100

    [KWinWtqWindow] Include resource name and -class in Name property

--- src/20-Services/Wtq.Services.KWin/KWinWtqWindow.cs
+++ src/20-Services/Wtq.Services.KWin/KWinWtqWindow.cs
@@ -21,7 +21,7 @@ public class KWinWtqWindow(
 	/// </summary>
 	public override bool IsValid { get; } = true;
 
-	public override string? Name => _window?.ResourceClass;
+	public override string? Name => $"{_window?.ResourceName} (resource class: {_window?.ResourceClass})";
 
 	public override string? Title => _window?.Caption;
 
