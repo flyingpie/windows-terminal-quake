0aefbef94c075c2665438ef7b25df70ee17b582c
Marco van den Oever <marco@flyingpie.nl>
2024-11-24 18:49:01 +0100

    Cleaned up app startup

--- src/30-Host/Wtq.Host.Linux/GlobalUsings.cs
+++ src/30-Host/Wtq.Host.Linux/GlobalUsings.cs
@@ -1,2 +1,3 @@
 global using Ardalis.GuardClauses;
 global using System.Threading.Tasks;
+global using Wtq.Utils;
--- src/30-Host/Wtq.Host.Linux/Program.cs
+++ src/30-Host/Wtq.Host.Linux/Program.cs
@@ -1,13 +1,9 @@
-using Wtq.Utils;
-
 namespace Wtq.Host.Linux;
 
 public static class Program
 {
 	public static async Task Main(string[] args)
 	{
-		Log.Configure();
-
-		await new WtqLinux().RunAsync().NoCtx();
+		await new WtqLinux().RunAsync(args).NoCtx();
 	}
 }
