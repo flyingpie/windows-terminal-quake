b90bcffa22fc74152dd00be875f51b0bb467cfd9
Marco van den Oever <marco@flyingpie.nl>
2024-11-25 01:24:19 +0100

    [KWin] Fixed shortcuts (#156)
    Turned the very much proof-of-concept KWin hotkey code into something closer to production code.
    
    Supports most keys and modifiers, and includes decent auto-cleanup of KWin shortcuts.

--- src/20-Services/Wtq.Services.KWin/KWinHotkeyService.cs
+++ src/20-Services/Wtq.Services.KWin/KWinHotkeyService.cs
@@ -1,61 +1,95 @@
-#pragma warning disable // PoC
-
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using Wtq.Configuration;
+using Wtq.Events;
 using Wtq.Services.KWin.DBus;
 
 namespace Wtq.Services.KWin;
 
-internal class KWinHotkeyService : IHostedService
+/// <summary>
+/// TODO: DBus-only shortcut registration (we did get listening to key press events working, actual registration proved more difficult).
+/// TODO: Fetch known WTQ shortcut names, instead of the fixed index-based names.
+/// </summary>
+internal sealed class KWinHotkeyService
+	: IDisposable, IHostedService
 {
-	private readonly IOptionsMonitor<WtqOptions> _opts;
-	private readonly IKWinClient _kwinClient;
+	/// <summary>
+	/// To de-register any left-over shortcuts on WTQ start, we need to know their names.<br/>
+	/// However, we don't have a nice way of finding out what those names are.<br/>
+	///
+	/// So instead, we use an index-based naming scheme, so we get deterministic names.
+	/// </summary>
+	private const int MaxShortcutCount = 50;
+
+	private readonly ILogger _log = Log.For<KWinHotkeyService>();
+	private readonly InitLock _init = new();
+
 	private readonly IDBusConnection _dbus;
 
+	private int _shortcutIndex;
+
 	public KWinHotkeyService(
-		IOptionsMonitor<WtqOptions> opts,
 		IKWinClient kwinClient,
-		IDBusConnection dbus)
+		IDBusConnection dbus,
+		IWtqBus bus)
 	{
-		_opts = opts;
-		_kwinClient = kwinClient;
-		_dbus = dbus;
+		_dbus = Guard.Against.Null(dbus);
+		_ = Guard.Against.Null(bus);
+
+		bus.OnEvent<WtqHotkeyDefinedEvent>(
+			async e =>
+			{
+				await InitAsync().NoCtx();
+
+				var name = GetShortcutName(_shortcutIndex++);
+
+				_log.LogInformation("Registering shortcut with name '{Name}', modifiers '{Modifiers}' and key '{Key}'", name, e.Modifiers, e.Key);
+
+				await kwinClient.RegisterHotkeyAsync(name, e.AppOptions?.Name ?? string.Empty, e.Modifiers, e.Key, CancellationToken.None).NoCtx();
+			});
+	}
+
+	public void Dispose()
+	{
+		_init.Dispose();
 	}
 
 	public async Task StartAsync(CancellationToken cancellationToken)
 	{
-		var kwinx = await _dbus.GetKWinServiceAsync();
-
-		var gl = kwinx.CreateKGlobalAccel("/kglobalaccel");
-		var comp = kwinx.CreateComponent("/component/kwin");
-		// var kwin = kwinx.CreateKWin("/org/kde/KWin");
-
-		// Clear.
-		for (var i = 0; i < 50; i++)
-		{
-			var resx1 = await gl.UnregisterAsync("kwin", $"wtq_hk1_{i:000}_scr_text");
-		}
-
-		await comp.CleanUpAsync();
-
-		// TODO: Although we haven't gotten shortcut registration to work reliably through direct DBus calls,
-		// we _can_ catch when shortcuts are being pressed/released.
-		// So dial down the JS part to just registration, remove the callback to WTQ part.
-
-		await _kwinClient.RegisterHotkeyAsync("wtq_hk1_000_scr", KeyModifiers.Control, Keys.Q, cancellationToken);
-
-		await _kwinClient.RegisterHotkeyAsync("wtq_hk1_001_scr", KeyModifiers.Control, Keys.D1, cancellationToken);
-		await _kwinClient.RegisterHotkeyAsync("wtq_hk1_002_scr", KeyModifiers.Control, Keys.D2, cancellationToken);
-		await _kwinClient.RegisterHotkeyAsync("wtq_hk1_003_scr", KeyModifiers.Control, Keys.D3, cancellationToken);
-		await _kwinClient.RegisterHotkeyAsync("wtq_hk1_004_scr", KeyModifiers.Control, Keys.D4, cancellationToken);
-		await _kwinClient.RegisterHotkeyAsync("wtq_hk1_005_scr", KeyModifiers.Control, Keys.D5, cancellationToken);
-		await _kwinClient.RegisterHotkeyAsync("wtq_hk1_006_scr", KeyModifiers.Control, Keys.D6, cancellationToken);
+		await InitAsync().NoCtx();
 	}
 
-	public Task StopAsync(CancellationToken cancellationToken)
+	public async Task StopAsync(CancellationToken cancellationToken)
 	{
-		// TODO: Cleanup.
-		return Task.CompletedTask;
+		await ResetShortcutsAsync().NoCtx();
+	}
+
+	private static string GetShortcutName(int index) => $"wtq_hotkey_{index:000}";
+
+	private async Task InitAsync()
+	{
+		await _init.InitAsync(ResetShortcutsAsync).NoCtx();
+	}
+
+	private async Task ResetShortcutsAsync()
+	{
+		_log.LogDebug("Removing shortcuts");
+
+		var kwin = await _dbus.GetKWinServiceAsync().NoCtx();
+
+		var gl = kwin.CreateKGlobalAccel("/kglobalaccel");
+		var comp = kwin.CreateComponent("/component/kwin");
+
+		// Remove individual shortcut registrations.
+		for (var i = 0; i < MaxShortcutCount; i++)
+		{
+			var name = GetShortcutName(i);
+
+			if (await gl.UnregisterAsync("kwin", name).NoCtx())
+			{
+				_log.LogDebug("Unregistered {Name}", name);
+			}
+		}
+
+		// Some GC-like flush.
+		await comp.CleanUpAsync().NoCtx();
 	}
 }
