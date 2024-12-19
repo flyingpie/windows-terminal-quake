ca7f6bbcd9392fe92f6d00b64a9384d14cab190c
Marco van den Oever <marco@flyingpie.nl>
2024-12-19 01:24:46 +0100

    [HotkeyOptions] IsAlt, IsCtrl and IsShift

--- src/10-Core/Wtq/Configuration/HotkeyOptions.cs
+++ src/10-Core/Wtq/Configuration/HotkeyOptions.cs
@@ -8,4 +8,54 @@ public sealed class HotkeyOptions
 	public Keys Key { get; set; }
 
 	public KeyModifiers Modifiers { get; set; }
+
+	public bool IsAlt
+	{
+		get => Modifiers.HasFlag(KeyModifiers.Alt);
+		set
+		{
+			if (value)
+			{
+				Modifiers |= KeyModifiers.Alt;
+			}
+			else
+			{
+				Modifiers &= ~KeyModifiers.Alt;
+			}
+		}
+	}
+
+	public bool IsCtrl
+	{
+		get => Modifiers.HasFlag(KeyModifiers.Control);
+		set
+		{
+			if (value)
+			{
+				Modifiers |= KeyModifiers.Control;
+			}
+			else
+			{
+				Modifiers &= ~KeyModifiers.Control;
+			}
+		}
+	}
+
+	public bool IsShift
+	{
+		get => Modifiers.HasFlag(KeyModifiers.Shift);
+		set
+		{
+			if (value)
+			{
+				Modifiers |= KeyModifiers.Shift;
+			}
+			else
+			{
+				Modifiers &= ~KeyModifiers.Shift;
+			}
+		}
+	}
+
+	public override string ToString() => $"{Modifiers} + {Key}";
 }
