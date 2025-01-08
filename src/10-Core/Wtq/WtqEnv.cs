fe5b8c95bd4cba14d4ccafb0b2a08f8fc56f3f25
Marco van den Oever <marco@flyingpie.nl>
2025-01-08 21:33:55 +0100

    Workspace

--- src/10-Core/Wtq/WtqEnv.cs
+++ src/10-Core/Wtq/WtqEnv.cs
@@ -9,6 +9,8 @@ public static class WtqEnv
 		public const string Config = "WTQ_CONFIG_FILE";
 
 		public const string LogLevel = "WTQ_LOG_LEVEL";
+
+		public const string LogToConsole = "WTQ_LOG_TO_CONSOLE";
 	}
 
 	/// <summary>
@@ -16,6 +18,13 @@ public static class WtqEnv
 	/// </summary>
 	public static string ConfigFile
 		=> Environment.GetEnvironmentVariable(Names.Config) ?? string.Empty;
+
+	/// <summary>
+	/// Whether to log to the console.
+	/// Should generally be turned off, as it can spam the journal on Linux.
+	/// </summary>
+	public static bool LogToConsole
+		=> Environment.GetEnvironmentVariable(Names.LogToConsole)?.ToString()?.Equals("true", StringComparison.OrdinalIgnoreCase) ?? false;
 
 	/// <summary>
 	/// Returns the requested log level, as specified by an environment variable.
