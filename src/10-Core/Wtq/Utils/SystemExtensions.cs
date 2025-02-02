b9e74a1ee23ffaa7cabd07a00ecbcfae38baba98
Marco van den Oever <marco@flyingpie.nl>
2025-02-02 22:51:16 +0100

    Support '~' in the 'WTQ_CONFIG_FILE' environment variable value

--- src/10-Core/Wtq/Utils/SystemExtensions.cs
+++ src/10-Core/Wtq/Utils/SystemExtensions.cs
@@ -2,6 +2,16 @@ namespace Wtq.Utils;
 
 public static class SystemExtensions
 {
+	public static string ExpandEnvVars(this string src)
+	{
+		src ??= string.Empty;
+
+		return Environment
+			.ExpandEnvironmentVariables(src)
+			?.Replace("~", WtqPaths.UserHome)
+			?? string.Empty;
+	}
+
 	public static string? EmptyOrWhiteSpaceToNull(this string? input)
 	{
 		if (string.IsNullOrWhiteSpace(input))
--- src/10-Core/Wtq/WtqEnv.cs
+++ src/10-Core/Wtq/WtqEnv.cs
@@ -14,15 +14,15 @@ public static class WtqEnv
 	/// <summary>
 	/// Returns path to the WTQ config file, as specified by an environment variable.
 	/// </summary>
-	public static string ConfigFile
-		=> Environment.GetEnvironmentVariable(Names.Config) ?? string.Empty;
+	public static string? ConfigFile
+		=> Environment.GetEnvironmentVariable(Names.Config)?.ExpandEnvVars()?.EmptyOrWhiteSpaceToNull();
 
 	/// <summary>
 	/// Whether to log to the console.
 	/// Should generally be turned off, as it can spam the journal on Linux.
 	/// </summary>
 	public static bool LogToConsole
-		=> Environment.GetEnvironmentVariable(Names.LogToConsole)?.ToString()?.Equals("true", StringComparison.OrdinalIgnoreCase) ?? false;
+		=> Environment.GetEnvironmentVariable(Names.LogToConsole)?.Equals("true", StringComparison.OrdinalIgnoreCase) ?? false;
 
 	/// <summary>
 	/// Returns the requested log level, as specified by an environment variable.
