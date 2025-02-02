497e338d370e09225c07ad5022cb1bbd999afedc
Marco van den Oever <marco@flyingpie.nl>
2025-02-02 23:33:50 +0100

    Restore app window locations when they're changed outside of WTQ (#165)

--- src/10-Core/Wtq/WtqApp.cs
+++ src/10-Core/Wtq/WtqApp.cs
@@ -23,6 +23,8 @@ public sealed class WtqApp : IAsyncDisposable
 	private readonly IWtqWindowResolver _windowResolver;
 	private readonly Worker _loop;
 
+	private Point? _lastLoc;
+
 	public WtqApp(
 		IOptionsMonitor<WtqOptions> opts,
 		IWtqAppToggleService toggler,
@@ -172,6 +174,8 @@ public sealed class WtqApp : IAsyncDisposable
 		}
 
 		await Window.MoveToAsync(location).NoCtx();
+
+		_lastLoc = location;
 	}
 
 	public async Task<bool> OpenAsync(ToggleModifiers mods = ToggleModifiers.None)
@@ -226,6 +230,11 @@ public sealed class WtqApp : IAsyncDisposable
 		// Check that if we have a process handle, the process is still active.
 		if (IsAttached)
 		{
+			if (_lastLoc != null)
+			{
+				await CheckAndRestoreWindowRectAsync(Window, _lastLoc.Value).NoCtx();
+			}
+
 			_log.LogTrace("Window handle '{Window}' for app '{App}' is still active, skipping update", Window, this);
 			return;
 		}
@@ -248,6 +257,34 @@ public sealed class WtqApp : IAsyncDisposable
 		_log.LogInformation("Got window for app {App}, attaching", this);
 
 		await AttachToWindowAsync(window).NoCtx();
+	}
+
+	/// <summary>
+	/// App windows can sometimes move around the screen due to external interaction.<br/>
+	/// For example, when the screen resolution changes, a monitor is (dis)connected, or when the shell restarts.<br/>
+	/// <br/>
+	/// Compares the current location of the app window, to the one we last set it to.<br/>
+	/// If they don't match, moves it back to where we last put it.
+	/// </summary>
+	private async Task CheckAndRestoreWindowRectAsync(WtqWindow window, Point lastLoc)
+	{
+		// Fetch current window location.
+		var rect = await window.GetWindowRectAsync().NoCtx();
+
+		// Check the distance between where we last left the window, and where it is now.
+		var dist = rect.Location.DistanceTo(lastLoc);
+
+		// Allow a little bit of drift, but restore location when the distance gets too large.
+		if (dist > 10)
+		{
+			_log.LogWarning(
+				"Window seems to have moved externally, restoring location (currently {CurrentLoc}, moving to {MovingToLoc}, distance of {Distance})",
+				rect,
+				lastLoc,
+				dist);
+
+			await MoveWindowAsync(lastLoc).NoCtx();
+		}
 	}
 
 	/// <summary>
