393cc8bf1dd3e5a083b138735c1a331071265f5a
Marco van den Oever <marco@flyingpie.nl>
2025-02-02 23:14:28 +0100

    Workspace

--- src/10-Core/Wtq/Utils/SystemExtensions.cs
+++ src/10-Core/Wtq/Utils/SystemExtensions.cs
@@ -2,8 +2,6 @@ namespace Wtq.Utils;
 
 public static class SystemExtensions
 {
-	private static readonly ILogger Log = Utils.Log.For(typeof(SystemExtensions));
-
 	public static string ExpandEnvVars(this string src)
 	{
 		src ??= string.Empty;
@@ -29,27 +27,5 @@ public static class SystemExtensions
 		ArgumentNullException.ThrowIfNull(values);
 
 		return string.Join(separator, values);
-	}
-
-	/// <summary>
-	/// Returns a rectangle from <paramref name="rects"/>, that does NOT intersect <paramref name="rectsToAvoid"/>.<br/>
-	/// Returns null if no non-intersecting rectangle could be found.
-	/// </summary>
-	public static Rectangle? FindWithNoIntersection(this Rectangle[] rects, Rectangle[] rectsToAvoid)
-	{
-		Log.LogDebug("Looking for rectangle within set '{Candidates}', that does not intersect '{Rect}'", rects.StringJoin(), rectsToAvoid);
-
-		foreach (var candidate in rects)
-		{
-			// if (candidate.IntersectsWith(rectsToAvoid))
-			// {
-			// 	Log.LogDebug("Candidate rectangle '' intersects with ");
-			// }
-		}
-
-		// var targetRect = rects
-		// 	.FirstOrDefault(r => !screenRects.Any(scr => scr.IntersectsWith(r)));
-
-		return null;
 	}
 }
