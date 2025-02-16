6310c4b84de01a9741a23d6e7e30b05de2a528eb
Marco van den Oever <marco@flyingpie.nl>
2025-02-16 12:45:40 +0100

    [Linux] Generate an example - if none exists - configuration using Konsole

--- src/10-Core/Wtq/Configuration/WtqOptionsPath.cs
+++ src/10-Core/Wtq/Configuration/WtqOptionsPath.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace Wtq.Configuration;
 
 public class WtqOptionsPath
@@ -15,6 +17,10 @@ public class WtqOptionsPath
 			path = WtqPaths.PreferredWtqConfigPath;
 			_log.LogInformation("No WTQ configuration found, generating example config at path '{Path}'", path);
 
+			// Make sure the directory exists.
+			Directory.CreateDirectory(System.IO.Path.GetDirectoryName(path)!);
+
+			// Write example config.
 			File.WriteAllText(path, GetExampleWtqConf());
 		}
 
@@ -23,16 +29,28 @@ public class WtqOptionsPath
 
 	public string Path { get; private set; }
 
-	// TODO: Linux equivalents.
+	/// <summary>
+	/// Returns example contents for "wtq.jsonc", that we can use to start off the user.<br/>
+	/// For use when WTQ starts with no settings file present.
+	/// </summary>
 	private string GetExampleWtqConf()
 	{
+		// On Linux, default to Konsole (as we currently only support KDE 5 & 6).
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+		{
+			_log.LogInformation("Running on Linux, using Konsole example config");
+			return Resources.Resources.wtq_example_konsole;
+		}
+
+		// On Windows, check if Windows Terminal is available. If so, use an example config with WT configured.
 		if (Os.ExistsOnPath("wt.exe"))
 		{
-			_log.LogInformation("Found Windows Terminal to be installed, using WT example config.");
+			_log.LogInformation("Found Windows Terminal to be installed, using WT example config");
 			return Resources.Resources.wtq_example_wt;
 		}
 
-		_log.LogInformation("Did not find Windows Terminal to be installed, using PowerShell example config.");
+		// On Windows, with (seemingly) no Windows Terminal installed, fallback to PowerShell.
+		_log.LogInformation("Did not find Windows Terminal to be installed, using PowerShell example config");
 		return Resources.Resources.wtq_example_ps;
 	}
 
