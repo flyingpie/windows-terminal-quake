6c550c68784cdc8c165482a161645b1846deeca4
Marco van den Oever <marco@flyingpie.nl>
2025-02-22 15:29:59 +0100

    Workspace

--- src/20-Services/Wtq.Services.Win32/Win32WtqWindow.cs
+++ src/20-Services/Wtq.Services.Win32/Win32WtqWindow.cs
@@ -105,19 +105,21 @@ public sealed class Win32WtqWindow(
 
 		_log.LogInformation("Setting taskbar icon visibility for process with main window handle '{Handle}'", handle);
 
-		// Get current window properties
-		var props = User32.GetWindowLong(handle, User32.GWLEXSTYLE);
+		Shell32.SetTaskbarIconVisible(handle, isVisible);
 
-		if (isVisible)
-		{
-			// Show
-			User32.SetWindowLong(handle, User32.GWLEXSTYLE, (props | User32.WSEXTOOLWINDOW) & User32.WSEXAPPWINDOW);
-		}
-		else
-		{
-			// Hide
-			User32.SetWindowLong(handle, User32.GWLEXSTYLE, (props | User32.WSEXTOOLWINDOW) & ~User32.WSEXAPPWINDOW);
-		}
+		// // Get current window properties
+		// var props = User32.GetWindowLong(handle, User32.GWLEXSTYLE);
+		//
+		// if (isVisible)
+		// {
+		// 	// Show
+		// 	User32.SetWindowLong(handle, User32.GWLEXSTYLE, (props | User32.WSEXTOOLWINDOW) & User32.WSEXAPPWINDOW);
+		// }
+		// else
+		// {
+		// 	// Hide
+		// 	User32.SetWindowLong(handle, User32.GWLEXSTYLE, (props | User32.WSEXTOOLWINDOW) & ~User32.WSEXAPPWINDOW);
+		// }
 
 		return Task.CompletedTask;
 	}
