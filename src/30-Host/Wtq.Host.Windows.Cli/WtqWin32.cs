404b557c0fcfcb4906c06b72026cbc4d93aaf1ce
Marco van den Oever <marco@flyingpie.nl>
2025-02-27 21:32:54 +0100

    Workspace

---
+++ src/30-Host/Wtq.Host.Windows.Cli/WtqWin32.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.DependencyInjection;
+using Wtq.Host.Base;
+using Wtq.Services.TrayIcon;
+using Wtq.Services.Win32;
+using Wtq.Services.WinForms;
+
+namespace Wtq.Host.Windows.Cli;
+
+public class WtqWin32 : WtqHostBase
+{
+	protected override void ConfigureServices(IServiceCollection services)
+	{
+		Guard.Against.Null(services);
+
+		services
+			.AddWin32WindowService()
+			.AddWinFormsHotkeyService()
+			.AddWinFormsScreenInfoProvider()
+
+			// New cross-platform tray icon.
+			//.AddTrayIcon()
+
+			// WinForms (Windows-only) tray icon, kept around should the cross-platform one causes issues.
+			.AddWinFormsTrayIcon()
+		;
+	}
+}
