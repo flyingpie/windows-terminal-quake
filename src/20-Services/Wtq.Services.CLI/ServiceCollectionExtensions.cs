6f48f4ee0789ee2ff4a2fa8e49f9842904f98d23
Marco van den Oever <marco@flyingpie.nl>
2025-06-11 23:51:26 +0200

    Workspace

--- src/20-Services/Wtq.Services.CLI/ServiceCollectionExtensions.cs
+++ src/20-Services/Wtq.Services.CLI/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using DeclarativeCommandLine.Extensions;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO.Pipes;
 using System.Net.Sockets;
 using Wtq.Services.CLI.Commands.Apps;
 
@@ -18,24 +19,7 @@ public static class ServiceCollectionExtensions
 			{
 				var handler = new SocketsHttpHandler()
 				{
-					ConnectCallback = async (ctx, ct) =>
-					{
-						var socketPath = "/tmp/wtq.sock";
-
-						// Define the type of socket we want, i.e. a UDS stream-oriented socket
-						var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.IP);
-
-						// Create a UDS endpoint using the socket path
-						var endpoint = new UnixDomainSocketEndPoint(socketPath);
-
-						// Connect to the server!
-						await socket.ConnectAsync(endpoint, ct);
-
-						// Wrap the socket in a NetworkStream and return it
-						// Setting ownsSocket: true means the NetworkStream will
-						// close and dispose the Socket when the stream is disposed
-						return new NetworkStream(socket, ownsSocket: true);
-					},
+					ConnectCallback = Os.IsWindows ? WindowsNamedPipe() : UnixSocket(),
 				};
 
 				return new HttpClient(handler)
@@ -44,4 +28,38 @@ public static class ServiceCollectionExtensions
 				};
 			});
 	}
+
+	private static Func<SocketsHttpConnectionContext, CancellationToken, ValueTask<Stream>> UnixSocket() =>
+		async (ctx, ct) =>
+		{
+			var socketPath = "/tmp/wtq.sock";
+
+			// Define the type of socket we want, i.e. a UDS stream-oriented socket
+			var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.IP);
+
+			// Create a UDS endpoint using the socket path
+			var endpoint = new UnixDomainSocketEndPoint(socketPath);
+
+			// Connect to the server!
+			await socket.ConnectAsync(endpoint, ct);
+
+			// Wrap the socket in a NetworkStream and return it
+			// Setting ownsSocket: true means the NetworkStream will
+			// close and dispose the Socket when the stream is disposed
+			return new NetworkStream(socket, ownsSocket: true);
+		};
+
+	private static Func<SocketsHttpConnectionContext, CancellationToken, ValueTask<Stream>> WindowsNamedPipe() =>
+		async (ctx, ct) =>
+		{
+			var stream = new NamedPipeClientStream(
+				serverName: ".",
+				pipeName: "wtq",
+				direction: PipeDirection.InOut,
+				options: PipeOptions.Asynchronous);
+
+			await stream.ConnectAsync(ct).NoCtx();
+
+			return stream;
+		};
 }
