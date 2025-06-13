401f1a35deea78a71c16530601c34e25765b4bf8
Marco van den Oever <marco@flyingpie.nl>
2025-06-13 02:21:31 +0200

    [Os] IsLinux & IsWindows

--- src/10-Core/Wtq/Utils/Os.cs
+++ src/10-Core/Wtq/Utils/Os.cs
@@ -6,6 +6,12 @@ public static class Os
 {
 	private static readonly ILogger Log = Utils.Log.For(typeof(Os));
 
+	public static bool IsLinux =>
+		RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+	public static bool IsWindows =>
+		RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
 	public static bool IsCallable(string fileName)
 	{
 		return RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
