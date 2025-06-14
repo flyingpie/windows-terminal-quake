d6e478254e62a36c636a04928081aa39e88aaddb
Marco van den Oever <marco@flyingpie.nl>
2025-06-15 00:22:58 +0200

    Workspace

--- src/20-Services/Wtq.Services.Win32/Native/Kernel32.cs
+++ src/20-Services/Wtq.Services.Win32/Native/Kernel32.cs
@@ -1,10 +1,39 @@
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace Wtq.Services.Win32.Native;
 
 public static class Kernel32
 {
+	public const int ATTACH_PARENT_PROCESS = -1;
+	public const int STD_OUTPUT_HANDLE = -11;
+	public const int STD_ERROR_HANDLE = -12;
+	public const int STD_INPUT_HANDLE = -10;
+
 	[DllImport("kernel32.dll", SetLastError = true)]
-	[return: MarshalAs(UnmanagedType.Bool)]
-	public static extern bool AllocConsole();
+	public static extern bool AttachConsole(int dwProcessId);
+
+	[DllImport("kernel32.dll", SetLastError = true)]
+	static extern IntPtr GetStdHandle(int nStdHandle);
+
+	[DllImport("kernel32.dll", SetLastError = true)]
+	static extern bool SetStdHandle(int nStdHandle, IntPtr handle);
+
+	public static void RedirectConsoleStreams()
+	{
+		var stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+		var safeStdOut = new Microsoft.Win32.SafeHandles.SafeFileHandle(stdOut, false);
+		var stdoutWriter = new StreamWriter(new FileStream(safeStdOut, FileAccess.Write))
+		{
+			AutoFlush = true,
+		};
+
+		Console.SetOut(stdoutWriter);
+		Console.SetError(stdoutWriter);
+
+		var stdIn = GetStdHandle(STD_INPUT_HANDLE);
+		var safeStdIn = new Microsoft.Win32.SafeHandles.SafeFileHandle(stdIn, false);
+		var stdinReader = new StreamReader(new FileStream(safeStdIn, FileAccess.Read));
+		Console.SetIn(stdinReader);
+	}
 }
--- src/30-Host/Wtq.Host.Windows.Cli/README.md
+++
@@ -1,6 +0,0 @@
-# Wtq.Host.Windows.Cli
-
-The Windows binary compiles to a WinExe image, which spawns a window (as opposed to interacting with the command line).
-
-For troubleshooting, it can be very useful to run WTQ from the command line. So the Wtq.Host.Windows.Cli project is a
-copy of Wtq.Host.Windows, but compiles to a regular Exe (that does work on the command line).
