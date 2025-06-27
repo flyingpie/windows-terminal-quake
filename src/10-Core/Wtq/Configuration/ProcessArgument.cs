347390361db809298748c73eb067e39d0f26dcb8
Marco van den Oever <marco@flyingpie.nl>
2025-06-27 02:10:02 +0200

    [ProcessArgument] Convenience constructor

--- src/10-Core/Wtq/Configuration/ProcessArgument.cs
+++ src/10-Core/Wtq/Configuration/ProcessArgument.cs
@@ -2,5 +2,14 @@ namespace Wtq.Configuration;
 
 public class ProcessArgument
 {
+	public ProcessArgument()
+	{
+	}
+
+	public ProcessArgument(string argument)
+	{
+		Argument = argument;
+	}
+
 	public string? Argument { get; set; }
 }
