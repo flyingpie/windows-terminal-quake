e032b951d95a28ed9a8cccf28639abff72884c8f
Marco van den Oever <marco@flyingpie.nl>
2025-06-28 12:21:45 +0200

    [KeySequence] Fixed 'Equals', added IsShiftImplied() and ToShortString()

--- src/10-Core/Wtq/Input/KeySequence.cs
+++ src/10-Core/Wtq/Input/KeySequence.cs
@@ -6,7 +6,7 @@ namespace Wtq.Input;
 public readonly struct KeySequence(
 	KeyModifiers modifiers,
 	string? keyChar,
-	KeyCode? keyCode)
+	KeyCode keyCode)
 	: IEquatable<KeySequence>
 {
 	/// <summary>
@@ -23,14 +23,13 @@ public readonly struct KeySequence(
 	/// <summary>
 	/// The pressed key (Q, 1, F1, etc.), as a virtual key code.
 	/// </summary>
-	public KeyCode? KeyCode { get; } = keyCode;
+	public KeyCode KeyCode { get; } = keyCode;
 
 	/// <summary>
 	/// Returns whether <see cref="KeyCode"/> is not empty.
 	/// </summary>
-	[MemberNotNullWhen(true, nameof(KeyCode))]
 	[JsonIgnore]
-	public bool HasKeyCode => KeyCode.HasValue && KeyCode != Wtq.Input.KeyCode.None;
+	public bool HasKeyCode => KeyCode != KeyCode.None;
 
 	/// <summary>
 	/// The pressed key, as a string, as translated by the keyboard layout.
@@ -66,12 +65,117 @@ public readonly struct KeySequence(
 		return false;
 	}
 
-	public bool Equals(KeySequence other) =>
-		Modifiers == other.Modifiers && KeyCode == other.KeyCode && KeyChar == other.KeyChar;
+	public bool Equals(KeySequence other)
+	{
+		// Modifiers have to be the same.
+		if (Modifiers != other.Modifiers)
+		{
+			return false;
+		}
+
+		// If a key character was present on both sides, and it matches, the key sequence can be considered the same.
+		if (HasKeyChar && other.HasKeyChar && KeyChar.Equals(other.KeyChar, StringComparison.OrdinalIgnoreCase))
+		{
+			return true;
+		}
+
+		// Alternatively, if a virtual key code was present on both sides, and it matches, also good.
+		if (HasKeyCode && other.HasKeyCode && KeyCode == other.KeyCode)
+		{
+			return true;
+		}
+
+		// If we get here, neither the key char nor the key code matched.
+		// Note that a key is required, we can't have a key sequence with only a modifier.
+		return false;
+	}
 
 	public override int GetHashCode() =>
 		HashCode.Combine((int)Modifiers, KeyCode, KeyChar);
 
 	public override string ToString() =>
 		$"Modifiers:'{Modifiers}' KeyChar:{(HasKeyChar ? $"'{KeyChar}'" : "<none>")} KeyCode:{(HasKeyCode ? $"'{KeyCode}'" : "<none>")}";
+
+	/// <summary>
+	/// Returns whether the "SHIFT"-modifier has caused the pressed key to emit a symbol, unrelated to the one that
+	/// would have been emitted, had shift not been pressed.<br/>
+	/// <br/>
+	/// For example: when pressing the "A" key without shift returns "a", with shift "A". These are different, but related.<br/>
+	/// Pressing the "1" key on the main row of a US ANSI keyboard without shift returns "1", with shift "!". These are different, and not related.<br/>
+	/// <br/>
+	/// This is not a perfect method, but we need it for sending hotkey registrations to KWin, as there, "SHIFT" is not considered
+	/// when the character already implies one.<br/>
+	/// In other words, sending "CTRL+SHIFT+!" to KWin, does not work, it has to be sent as "CTRL+!".<br/>
+	/// <br/>
+	/// Nicer methods would probably require more access to the active keyboard layout and character mapping, which we don't have.
+	/// </summary>
+	public bool IsShiftImplied()
+	{
+		// The "shift" key must be part of the active modifiers.
+		if (!Modifiers.HasShift())
+		{
+			return false;
+		}
+
+		// We can skip non-character keys.
+		if (KeyChar == null)
+		{
+			return false;
+		}
+
+		// Don't consider key chars that are referring to a non-character key, like "Tab", or "F1".
+		if (KeyChar.Length > 1)
+		{
+			return false;
+		}
+
+		return KeyChar.All(c => !char.IsUpper(c) && !char.IsLower(c));
+	}
+
+	public string ToShortString()
+	{
+		var sb = new StringBuilder();
+
+		// Modifier
+		foreach (var m in new[] { KeyModifiers.Control, KeyModifiers.Alt, KeyModifiers.Shift, KeyModifiers.Super, KeyModifiers.Numpad, })
+		{
+			if (!Modifiers.HasFlag(m))
+			{
+				continue;
+			}
+
+			if (sb.Length > 0)
+			{
+				sb.Append('+');
+			}
+
+			sb.Append(m);
+		}
+
+		// Key char
+		// KWin uses key characters, so prefer that one if we have it right in the settings.
+		if (HasKeyChar)
+		{
+			if (sb.Length > 0)
+			{
+				sb.Append('+');
+			}
+
+			sb.Append(KeyChar);
+		}
+
+		// Alternatively, if no key character is present, map the key code.
+		// This is not a perfect method, as the character depends on the keyboard layout, which I haven't found a way to access yet.
+		else if (HasKeyCode)
+		{
+			if (sb.Length > 0)
+			{
+				sb.Append('+');
+			}
+
+			sb.Append(KeyCode);
+		}
+
+		return sb.ToString();
+	}
 }
