5208fb6c05705ec9a533d1222f8b9dae8eb3eb38
Marco van den Oever <marco@flyingpie.nl>
2025-06-29 17:02:09 +0200

    [SystemExtensions] GetFileNameWithoutExtension

--- src/10-Core/Wtq/Utils/SystemExtensions.cs
+++ src/10-Core/Wtq/Utils/SystemExtensions.cs
@@ -4,13 +4,6 @@ namespace Wtq.Utils;
 
 public static class SystemExtensions
 {
-	public static TValue JsonDeepClone<TValue>(this TValue value)
-	{
-		var json = JsonSerializer.Serialize(value);
-
-		return JsonSerializer.Deserialize<TValue>(json)!;
-	}
-
 	/// <summary>
 	/// "" => null<br/>
 	/// " " => null<br/>
@@ -19,6 +12,20 @@ public static class SystemExtensions
 	/// "not-null" => "not-null".<br/>
 	/// </summary>
 	public static string? EmptyOrWhiteSpaceToNull(this string? input) => string.IsNullOrWhiteSpace(input) ? null : input;
+
+	public static string GetFileNameWithoutExtension(this string fileName)
+	{
+		Guard.Against.NullOrWhiteSpace(fileName);
+
+		return Path.GetFileNameWithoutExtension(fileName)?.EmptyOrWhiteSpaceToNull() ?? fileName;
+	}
+
+	public static TValue JsonDeepClone<TValue>(this TValue value)
+	{
+		var json = JsonSerializer.Serialize(value);
+
+		return JsonSerializer.Deserialize<TValue>(json)!;
+	}
 
 	public static IEnumerable<ValidationResult> Validate(this IValidatableObject validatable)
 	{
