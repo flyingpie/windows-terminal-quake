f312b9c380b907186ec5cf1c7a8fecf7f1ae465e
Marco van den Oever <marco@flyingpie.nl>
2025-07-01 00:13:59 +0200

    [HotkeyRegistration] Removed 'HotkeyDefinedEvent', and made hotkey modules directly responsible for watching for settings changes

--- src/20-Services/Wtq.Services.WinForms/WinFormsHotkeyService.cs
+++ src/20-Services/Wtq.Services.WinForms/WinFormsHotkeyService.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Options;
+using Wtq.Configuration;
 using Wtq.Input;
 using Wtq.Services.WinForms.Native;
 using KeyModifiers = Wtq.Services.WinForms.Native.KeyModifiers;
@@ -9,27 +11,29 @@ public sealed class WinFormsHotkeyService : WtqHostedService
 {
 	private readonly ILogger _log = Log.For<WinFormsHotkeyService>();
 
-	public WinFormsHotkeyService(IWtqBus bus)
-	{
-		Guard.Against.Null(bus);
+	private readonly IWtqBus _bus;
 
-		// TODO: Handle suspend/resume
-		bus.OnEvent<WtqHotkeyDefinedEvent>(e =>
+	public WinFormsHotkeyService(IOptionsMonitor<WtqOptions> opts, IWtqBus bus)
+	{
+		_bus = Guard.Against.Null(bus);
+
+		// Update registrations every time the settings file is reloaded.
+		opts.OnChange((_, _) =>
 		{
-			if (!e.Sequence.HasKeyCode)
+			// Apps
+			foreach (var app in opts.CurrentValue.Apps)
 			{
-				_log.LogWarning("Key chars are not supported in the WinForms hotkey registration, please switch to the SharpHook one.");
-				return Task.CompletedTask;
+				foreach (var hk in app.Hotkeys)
+				{
+					Register(hk.Sequence);
+				}
 			}
 
-			var mods = (KeyModifiers)e.Sequence.Modifiers;
-			var key = (Keys)e.Sequence.KeyCode;
-
-			_log.LogInformation("Registering Hotkey '{Sequence}'", e.Sequence);
-
-			HotkeyManager.RegisterHotkey(key, mods);
-
-			return Task.CompletedTask;
+			// Global
+			foreach (var hk in opts.CurrentValue.Hotkeys)
+			{
+				Register(hk.Sequence);
+			}
 		});
 
 		HotkeyManager.HotkeyPressed += (s, a) =>
@@ -38,6 +42,22 @@ public sealed class WinFormsHotkeyService : WtqHostedService
 
 			bus.Publish(new WtqHotkeyPressedEvent(keySeq));
 		};
+	}
+
+	private void Register(KeySequence sequence)
+	{
+		if (!sequence.HasKeyCode)
+		{
+			_log.LogWarning("Key chars are not supported in the WinForms hotkey registration, please switch to the SharpHook one.");
+			return;
+		}
+
+		var mods = (KeyModifiers)sequence.Modifiers;
+		var key = (Keys)sequence.KeyCode;
+
+		_log.LogInformation("Registering Hotkey '{Sequence}'", sequence);
+
+		HotkeyManager.RegisterHotkey(key, mods);
 	}
 
 	protected override ValueTask OnDisposeAsync()
