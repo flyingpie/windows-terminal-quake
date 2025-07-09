6f52ed844063914703d941bb004287fbdc645fdd
Marco van den Oever <marco@flyingpie.nl>
2025-07-09 02:40:40 +0200

    [KWin] Updated tests

---
+++ src/20-Services/Wtq.Services.KWin.UnitTest/AssemblyInit.cs
@@ -0,0 +1,13 @@
+using Wtq.Utils;
+
+namespace Wtq.Services.KWin.UnitTest;
+
+[TestClass]
+public static class AssemblyInit
+{
+	[AssemblyInitialize]
+	public static void Setup(TestContext ctx)
+	{
+		Log.Configure();
+	}
+}
