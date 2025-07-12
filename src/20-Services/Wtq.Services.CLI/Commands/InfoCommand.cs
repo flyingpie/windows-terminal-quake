3138a2818520cff72b0a7385d16a06a704394cfe
Marco van den Oever <marco@flyingpie.nl>
2025-07-12 20:16:42 +0200

    [CLI] wtq info - Shows some paths, such as where the logs are stored

---
+++ src/20-Services/Wtq.Services.CLI/Commands/InfoCommand.cs
@@ -0,0 +1,20 @@
+namespace Wtq.Services.CLI.Commands;
+
+[Command]
+public class InfoCommand : ICommand
+{
+	public void Execute()
+	{
+		Console.WriteLine($"-- Paths --------------------------------------------------------");
+		Console.WriteLine($"{nameof(WtqPaths.AppData)}:....................{WtqPaths.AppData}");
+		Console.WriteLine($"{nameof(WtqPaths.AppDataWtq)}:.................{WtqPaths.AppDataWtq}");
+		Console.WriteLine($"{nameof(WtqPaths.PreferredWtqConfigPath)}:.....{WtqPaths.PreferredWtqConfigPath}");
+		Console.WriteLine($"{nameof(WtqPaths.UserHome)}:...................{WtqPaths.UserHome}");
+		Console.WriteLine($"{nameof(WtqPaths.GetWtqAppDir)}:...............{WtqPaths.GetWtqAppDir()}");
+		Console.WriteLine($"{nameof(WtqPaths.WtqConfigFromEnvVar)}:........{WtqPaths.WtqConfigFromEnvVar}");
+		Console.WriteLine($"{nameof(WtqPaths.WtqConfigPaths)}:.............{string.Join("\n-", WtqPaths.WtqConfigPaths)}");
+		Console.WriteLine($"{nameof(WtqPaths.GetWtqLogDir)}:...............{WtqPaths.GetWtqLogDir()}");
+		Console.WriteLine($"{nameof(WtqPaths.GetWtqTempDir)}:..............{WtqPaths.GetWtqTempDir()}");
+		Console.WriteLine($"-- /Paths -------------------------------------------------------");
+	}
+}
