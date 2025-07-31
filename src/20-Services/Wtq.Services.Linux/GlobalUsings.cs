5ddf428c135ced0daadce133c11b4bfe186e2d16
Marco van den Oever <marco@flyingpie.nl>
2025-08-01 01:38:54 +0200

    Added 'Services.Linux' project for process factories

---
+++ src/20-Services/Wtq.Services.Linux/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Ardalis.GuardClauses;
+global using System;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.IO;
+global using Wtq.Utils;
