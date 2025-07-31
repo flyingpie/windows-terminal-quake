6adce67a877e3f1362165474503a336951265af0
Marco van den Oever <marco@flyingpie.nl>
2025-08-01 01:31:09 +0200

    Simple file system abstraction for mocking

---
+++ src/10-Core/Wtq/Utils/Fs.cs
@@ -0,0 +1,17 @@
+namespace Wtq.Utils;
+
+public class Fs : IFs
+{
+	public static IFs Inst { get; set; } = new Fs();
+
+	public string CreateDir(string path)
+	{
+		Directory.CreateDirectory(path);
+
+		return path;
+	}
+
+	public bool DirExists(string path) => Directory.Exists(path);
+
+	public bool FileExists(string path) => File.Exists(path);
+}
