2d71ebefccc4f810cfc90dd9f3c9bea42b4e182a
Marco van den Oever <marco@flyingpie.nl>
2025-08-10 19:44:37 +0200

    [PlatformService] Refactor a bunch of OS- and environment- specific stuff into IPlatformService and its implementations. (#208)

--- src/20-Services/Wtq.Services.WinForms/WinFormsTrayIconService.cs
+++ src/20-Services/Wtq.Services.WinForms/WinFormsTrayIconService.cs
@@ -2,16 +2,21 @@ namespace Wtq.Services.WinForms;
 
 internal sealed class WinFormsTrayIconService(
 	IHostApplicationLifetime lifetime,
-	IWtqBus bus)
+	IPlatformService platform,
+	IWtqBus bus,
+	TrayIconUtil trayIconUtil)
 	: WtqHostedService
 {
 	private readonly IHostApplicationLifetime _lifetime = Guard.Against.Null(lifetime);
+	private readonly IPlatformService _platform = Guard.Against.Null(platform);
 	private readonly IWtqBus _bus = Guard.Against.Null(bus);
+	private readonly TrayIconUtil _trayIconUtil = Guard.Against.Null(trayIconUtil);
+
 	private TrayIcon? _icon;
 
 	protected override Task OnStartAsync(CancellationToken cancellationToken)
 	{
-		_icon = new TrayIcon(_lifetime, _bus);
+		_icon = new TrayIcon(_lifetime, _bus, _platform, _trayIconUtil);
 
 		return Task.CompletedTask;
 	}
--- src/30-Host/Wtq.Host.Windows/WtqWin32.cs
+++ src/30-Host/Wtq.Host.Windows/WtqWin32.cs
@@ -2,12 +2,17 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Wtq.Configuration;
 using Wtq.Host.Base;
+using Wtq.Services;
+using Wtq.Services.Win32v2;
 using Wtq.Services.WinForms;
 
 namespace Wtq.Host.Windows;
 
 public class WtqWin32 : WtqHostBase
 {
+	protected override IPlatformService CreatePlatformService() =>
+		new WindowsPlatformService();
+
 	protected override void ConfigureServices(IServiceCollection services)
 	{
 		Guard.Against.Null(services);
