2d77745f2ce0ff19016766db5443ff1bcf50680b
Marco van den Oever <marco@flyingpie.nl>
2025-08-28 23:58:50 +0200

    Packaging cleanup

---
+++ pkg/flatpak/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+flatpak-builder build-dir --user --force-clean --install --repo=../../repo nl.flyingpie.wtq.yml
--- src/01-Build/NukeBuild/Build.Manifests.cs
+++ src/01-Build/NukeBuild/Build.Manifests.cs
@@ -2,9 +2,12 @@
 // ReSharper disable All
 
 using Nuke.Common;
+using Nuke.Common.Tools.DotNet;
 using System;
 using System.IO;
 using System.Security.Cryptography;
+using System.Text.Json;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 /// <summary>
 /// Manifests for upstream repositories.
@@ -14,10 +17,52 @@ public sealed partial class Build
 	/// <summary>
 	/// Scoop manifest.
 	/// </summary>
+	private Target CreateFlatpakManifest => _ => _
+		.Executes(async () =>
+		{
+			var templateRoot = PkgDirectory / "flatpak" / "_template";
+			var manifestRoot = PkgDirectory / "flatpak" / SemVerVersion;
+			var prefix = "nl.flyingpie.wtq";
+
+			if (Directory.Exists(manifestRoot)) { Directory.Delete(manifestRoot, true); }
+
+			Directory.CreateDirectory(manifestRoot);
+
+			var installerFn = $"{prefix}.desktop";
+			var localeFn = $"{prefix}.metainfo.xml";
+			var mainFn = $"{prefix}.yml";
+
+			var fns = new string[] { installerFn, localeFn, mainFn };
+
+			foreach (var fn in fns)
+			{
+				var tpl = await File.ReadAllTextAsync(templateRoot / fn);
+				var target = manifestRoot / fn;
+
+				var manifest = tpl
+					.Replace("$GH_RELEASE_VERSION$", GitHubRelease, StringComparison.OrdinalIgnoreCase)
+					.Replace("$GIT_COMMIT$", GitRepository.Commit, StringComparison.OrdinalIgnoreCase)
+					.Replace("$PACKAGE_VERSION$", SemVerVersion, StringComparison.OrdinalIgnoreCase)
+					.Replace("$RELEASE_DATE$", DateTimeOffset.UtcNow.ToString("yyyy-MM-dd"), StringComparison.OrdinalIgnoreCase);
+
+				await File.WriteAllTextAsync(target, manifest);
+			}
+
+			var nugetSources = Nupkg.FromNupkgDir(NupkgsDirectory);
+			var nugetSourcesJson = manifestRoot / "nuget-sources.json";
+
+			await File.WriteAllTextAsync(nugetSourcesJson, JsonSerializer.Serialize(nugetSources, Nupkg.JsonSerializerOptions));
+		});
+
+	/// <summary>
+	/// Scoop manifest.
+	/// </summary>
 	private Target CreateScoopManifest => _ => _
 		.Executes(async () =>
 		{
-			var tpl = await File.ReadAllTextAsync(RootDirectory / "scoop" / "_template.json");
+			var scoop = PkgDirectory / "scoop";
+
+			var tpl = await File.ReadAllTextAsync(scoop / "_template.json");
 			var sha256 = Convert.ToHexString(await SHA256.HashDataAsync(File.OpenRead(PathToWin64SelfContainedZip))).ToLowerInvariant();
 
 			var manifest = tpl
@@ -25,9 +70,9 @@ public sealed partial class Build
 				.Replace("$PACKAGE_VERSION$", SemVerVersion, StringComparison.OrdinalIgnoreCase)
 				.Replace("$SELF_CONTAINED_SHA256$", sha256, StringComparison.OrdinalIgnoreCase);
 
-			await File.WriteAllTextAsync(RootDirectory / "scoop" / "wtq-latest.json", manifest);
-			await File.WriteAllTextAsync(RootDirectory / "scoop" / "wtq-nightly.json", manifest);
-			await File.WriteAllTextAsync(RootDirectory / "scoop" / $"wtq-{SemVerVersion}.json", manifest);
+			await File.WriteAllTextAsync(scoop / "wtq-latest.json", manifest);
+			await File.WriteAllTextAsync(scoop / "wtq-nightly.json", manifest);
+			await File.WriteAllTextAsync(scoop / $"wtq-{SemVerVersion}.json", manifest);
 		});
 
 	/// <summary>
@@ -36,8 +81,8 @@ public sealed partial class Build
 	private Target CreateWinGetManifest => _ => _
 		.Executes(async () =>
 		{
-			var templateRoot = RootDirectory / "winget" / "_template";
-			var manifestRoot = RootDirectory / "winget" / SemVerVersion;
+			var templateRoot = PkgDirectory / "winget" / "_template";
+			var manifestRoot = PkgDirectory / "winget" / SemVerVersion;
 			var prefix = "flyingpie.windows-terminal-quake";
 			var sha256 = Convert.ToHexString(await SHA256.HashDataAsync(File.OpenRead(PathToWin64SelfContainedZip))).ToLowerInvariant();
 
@@ -49,10 +94,7 @@ public sealed partial class Build
 			var localeFn = $"{prefix}.locale.en-US.yaml";
 			var mainFn = $"{prefix}.yaml";
 
-			var fns = new string[]
-			{
-				installerFn, localeFn, mainFn
-			};
+			var fns = new string[] { installerFn, localeFn, mainFn };
 
 			foreach (var fn in fns)
 			{
