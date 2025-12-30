35193f3312589088c0daf2cf85b5db46ef641499
Marco van den Oever <marco@flyingpie.nl>
2025-12-30 19:52:29 +0100

    Updated debug info (both in CLI and GUI)

--- src/20-Services/Wtq.Services.CLI/Commands/InfoCommand.cs
+++ src/20-Services/Wtq.Services.CLI/Commands/InfoCommand.cs
@@ -1,8 +1,12 @@
+using Microsoft.Extensions.Options;
+using Wtq.Configuration;
+
 namespace Wtq.Services.CLI.Commands;
 
 [Command(Parent = typeof(AppRootCommand))]
-public class InfoCommand(IPlatformService platform) : ICommand
+public class InfoCommand(IOptions<WtqOptions> opts, IPlatformService platform) : ICommand
 {
+	[SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "MvdO: So the strings line up nicely.")]
 	public void Execute()
 	{
 		Console.WriteLine($"-- App ----------------------------------------------------------");
@@ -11,23 +15,25 @@ public class InfoCommand(IPlatformService platform) : ICommand
 		Console.WriteLine($"File version:..................................{WtqConstants.AppFileVersion}");
 		Console.WriteLine($"Informational version:.........................{WtqConstants.AppInformationalVersion}");
 		Console.WriteLine($"Build configuration:...........................{WtqConstants.BuildConfiguration}");
-		Console.WriteLine($"Build date:....................................{WtqConstants.BuildDate.ToString("s")}");
+		Console.WriteLine($"Build date:....................................{WtqConstants.BuildDate.ToIso8601()}");
 		Console.WriteLine($"Git branch:....................................{WtqConstants.GitBranch}");
 		Console.WriteLine($"Git commit:....................................{WtqConstants.GitCommit}");
 		Console.WriteLine($"OS color mode:.................................{platform.OsColorMode}");
+		Console.WriteLine($"-- API ----------------------------------------------------------");
+		Console.WriteLine($"API enabled:...................................{opts.Value.Api?.Enable ?? false}");
+		Console.WriteLine($"API endpoints:.................................{string.Join(", ", opts.Value.Api?.Urls ?? platform.DefaultApiUrls)}");
 
-		Console.WriteLine($"-- Paths --------------------------------------------------------");
+		Console.WriteLine($"-- Config paths -------------------------------------------------");
 		Console.WriteLine($"PreferredPathToWtqConfig:......................{platform.PreferredPathWtqConfig}");
-
 		Console.WriteLine($"PathToWtqConf:.................................{platform.PathToWtqConf}");
 		Console.WriteLine($"PathToWtqConfDir:..............................{platform.PathToWtqConfDir}");
-
-		Console.WriteLine($"Paths to WTQ configs");
-		foreach (var p in platform.PathsToWtqConfs)
+		Console.WriteLine($"Paths to WTQ configs:..........................{platform.PathsToWtqConfs.First()}");
+		foreach (var p in platform.PathsToWtqConfs.Skip(1))
 		{
-			Console.WriteLine($" - {p}{(Fs.Inst.FileExists(p) ? " (exists)" : string.Empty)}");
+			Console.WriteLine($"...............................................{p}{(Fs.Inst.FileExists(p) ? " (exists)" : string.Empty)}");
 		}
 
+		Console.WriteLine($"-- Misc paths ---------------------------------------------------");
 		Console.WriteLine($"PathToAppDir:..................................{platform.PathToAppDir}");
 		Console.WriteLine($"PathToAssetsDir:...............................{platform.PathToAssetsDir}");
 		Console.WriteLine($"PathToLogsDir:.................................{platform.PathToLogsDir}");
@@ -35,7 +41,6 @@ public class InfoCommand(IPlatformService platform) : ICommand
 		Console.WriteLine($"PathToTrayIconDark:............................{platform.PathToTrayIconDark}");
 		Console.WriteLine($"PathToTrayIconLight:...........................{platform.PathToTrayIconLight}");
 		Console.WriteLine($"PathToUserHomeDir:.............................{platform.PathToUserHomeDir}");
-
 		Console.WriteLine($"-----------------------------------------------------------------");
 	}
 }
