17a8351ae42944144660db74272408aa16a1ad56
Marco van den Oever <marco@flyingpie.nl>
2026-04-25 21:14:53 +0200

    refactor: move WtqPhotinoBlazorApp into separate file

--- src/20-Services/Wtq.Services.UI/WtqUIHost.cs
+++ src/20-Services/Wtq.Services.UI/WtqUIHost.cs
@@ -1,7 +1,4 @@
 using System.IO;
-using Microsoft.Extensions.DependencyInjection;
-using Photino.Blazor;
-using Photino.NET;
 
 namespace Wtq.Services.UI;
 
@@ -21,65 +18,5 @@ public class WtqUIHost(IPlatformService platform, WtqPhotinoBlazorApp app)
 			.SetTitle("WTQ - Main Window");
 
 		app.Run();
-	}
-}
-
-public class WtqPhotinoBlazorApp
-{
-	public WtqPhotinoBlazorApp(IServiceProvider services)
-	{
-		var rootComponents = new RootComponentList();
-		rootComponents.Add<App>("app");
-
-		Services = services;
-		RootComponents = Services.GetService<BlazorWindowRootComponents>();
-		MainWindow = Services.GetService<PhotinoWindow>();
-		WindowManager = Services.GetService<PhotinoWebViewManager>();
-		MainWindow
-			.SetTitle("Photino.Blazor App")
-			.SetUseOsDefaultSize(false)
-			.SetUseOsDefaultLocation(false)
-			.SetWidth(1000)
-			.SetHeight(900)
-			.SetLeft(450)
-			.SetTop(100);
-
-		MainWindow.RegisterCustomSchemeHandler(
-			PhotinoWebViewManager.BlazorAppScheme,
-			new(HandleWebRequest)
-		);
-		foreach ((Type, string) rootComponent in rootComponents)
-		{
-			RootComponents.Add(rootComponent.Item1, rootComponent.Item2);
-		}
-	}
-
-	public IServiceProvider Services { get; }
-
-	public BlazorWindowRootComponents RootComponents { get; }
-
-	public PhotinoWindow MainWindow { get; }
-
-	public PhotinoWebViewManager WindowManager { get; }
-
-	public void Run()
-	{
-		if (string.IsNullOrWhiteSpace(MainWindow.StartUrl))
-		{
-			MainWindow.StartUrl = "/";
-		}
-
-		WindowManager.Navigate(MainWindow.StartUrl);
-		MainWindow.WaitForClose();
-	}
-
-	private Stream HandleWebRequest(
-		object sender,
-		string scheme,
-		string url,
-		out string contentType
-	)
-	{
-		return WindowManager.HandleWebRequest(sender, scheme, url, out contentType);
 	}
 }
